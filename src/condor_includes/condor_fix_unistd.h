00837b3699cc23e0e115c4572852d4d79d2bcfe9
condor <condor>
1994-06-23 15:13:44 +0000

    1. Add SUNOS41 to existing conditional compilation directive. 2. Change sbrk()'s argument to ssize_t.

--- src/condor_includes/condor_fix_unistd.h
+++ src/condor_includes/condor_fix_unistd.h
@@ -7,16 +7,21 @@
 /*
   For some reason the g++ include files on Ultrix 4.3 fail to provide
   these prototypes, even though the Ultrix 4.2 versions did...
+  Once again, OSF1 also chokes, unless _AES_SOURCE(?) is defined JCP
 */
-#if defined(ULTRIX43)
+#if defined(ULTRIX43) || defined(SUNOS41) || defined(OSF1)
 
 #if defined(__cplusplus)
 extern "C" {
 #endif
 
+#if defined(SUNOS41)
+	typedef unsigned long ssize_t;
+#endif
+
 #if defined(__STDC__) || defined(__cplusplus)
 	int symlink( const char *, const char * );
-	char *sbrk( int );
+	void *sbrk( ssize_t );
 	int gethostname( char *, int );
 #else
 	int symlink();
