b93c33a3868c071f2c6fb4525dd7d0c8173b8bee
tannenba <tannenba>
1997-06-18 18:19:06 +0000

    changed some dprintf warning messages to D_FULLDEBUG

--- src/condor_ckpt_server/network2.c
+++ src/condor_ckpt_server/network2.c
@@ -291,7 +291,7 @@ char* getserveraddr()
 	if (server) {
 		h = gethostbyname(server);
 	} else {
-		dprintf(D_ALWAYS, "CKPT_SERVER Not defined in the config file.\n");
+		dprintf(D_FULLDEBUG, "CKPT_SERVER Not defined in the config file.\n");
 		return NULL;
 	}
 	if (h == NULL) {
