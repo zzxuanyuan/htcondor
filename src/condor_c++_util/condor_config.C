30814cfd91a760590b0920d9b5e618595751f40f
jbasney <jbasney>
1997-09-11 19:50:20 +0000

    initial port to WIN32

--- src/condor_c++_util/condor_config.C
+++ src/condor_c++_util/condor_config.C
@@ -47,13 +47,16 @@
 #include <stdio.h>
 #include <string.h>
 #include <ctype.h>
+#if !defined(WIN32)
 #include <pwd.h>
 #include <netdb.h>
+#endif
+#include "condor_common.h"
 #include "debug.h"
 #include "clib.h"
 #include "condor_sys.h"
 #include "condor_config.h"
-#include "util_lib_proto.h"
+#include "condor_string.h"
 
 #if defined(__cplusplus)
 extern "C" {
@@ -67,7 +70,7 @@ char *get_op_sys();
 int SetSyscalls(int);
 #if defined(LINUX) || defined(HPUX9)
     int gethostname(char*, unsigned int);
-#else
+#elif !defined(WIN32)
     int gethostname(char*, int);
 #endif
 
@@ -131,7 +134,9 @@ int
 real_config(const char *env_name, const char *file_name,
 			const char *local_name, ClassAd *classAd)
 {
+#if !defined(WIN32)
 	struct passwd	*pw, *getpwnam();
+#endif
 	char			*ptr;
 	int				rval, fd;
 	char			hostname[1024];
@@ -146,9 +151,11 @@ real_config(const char *env_name, const char *file_name,
 		*/
 	scm = SetSyscalls( SYS_LOCAL | SYS_UNRECORDED );
 
+#if !defined(WIN32)
 	if( (pw=getpwnam( "condor" )) ) {
 		tilde = strdup( pw->pw_dir );
 	} 
+#endif
 
 		// Find location of condor_config file
 
@@ -293,7 +300,9 @@ real_config(const char *env_name, const char *file_name,
 		}
 	}
 
+#if !defined(WIN32)
 	(void)endpwent();
+#endif
 	(void)SetSyscalls( scm );
 	return 0;
 }
@@ -329,7 +338,8 @@ macro_expand( char *str )
 ** Return non-zero iff the named configuration parameter contains the given
 ** pattern.  
 */
-boolean( char *parameter, char *pattern )
+int
+param_in_pattern( char *parameter, char *pattern )
 {
 	char	*argv[512];
 	int		argc;
@@ -351,6 +361,62 @@ boolean( char *parameter, char *pattern )
 	return 0;
 }
 
+
+#if defined(WIN32)
+
+char *
+get_arch()
+{
+	static char answer[1024];	
+	SYSTEM_INFO info;
+	GetSystemInfo(&info);
+	switch(info.wProcessorArchitecture) {
+	case PROCESSOR_ARCHITECTURE_INTEL:
+		sprintf(answer, "INTEL");
+		break;
+	case PROCESSOR_ARCHITECTURE_MIPS:
+		sprintf(answer, "MIPS");
+		break;
+	case PROCESSOR_ARCHITECTURE_ALPHA:
+		sprintf(answer, "ALPHA");
+		break;
+	case PROCESSOR_ARCHITECTURE_PPC:
+		sprintf(answer, "PPC");
+		break;
+	case PROCESSOR_ARCHITECTURE_UNKNOWN:
+	default:
+		sprintf(answer, "UNKNOWN");
+		break;
+	}
+
+	return answer;
+}
+
+char *
+get_op_sys()
+{
+	static char answer[1024];
+	OSVERSIONINFO info;
+	info.dwOSVersionInfoSize = sizeof(OSVERSIONINFO);
+	switch(info.dwPlatformId) {
+	case VER_PLATFORM_WIN32s:
+		sprintf(answer, "WIN32s%d%d", info.dwMajorVersion, info.dwMinorVersion);
+		break;
+	case VER_PLATFORM_WIN32_WINDOWS:
+		sprintf(answer, "WIN32%d%d", info.dwMajorVersion, info.dwMinorVersion);
+		break;
+	case VER_PLATFORM_WIN32_NT:
+		sprintf(answer, "WINNT%d%d", info.dwMajorVersion, info.dwMinorVersion);
+		break;
+	default:
+		sprintf(answer, "UNKNOWN");
+		break;
+	}
+
+	return answer;
+}
+
+#else
 /* uname() is POSIX, so this should work on all platforms.  -Jim */
 #include <sys/utsname.h>
 
@@ -380,6 +446,8 @@ get_op_sys()
 	strcat( answer, buf.release );
 	return answer;
 }
+
+#endif
 
 #if defined(__cplusplus)
 }
