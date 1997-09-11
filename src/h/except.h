17d3022ba2347caa2e7caf8ae38b5c1ae15a085e
jbasney <jbasney>
1997-09-11 21:17:16 +0000

    add _EXCEPT_ prototype

--- src/h/except.h
+++ src/h/except.h
@@ -52,6 +52,7 @@ extern int	_EXCEPT_Line;			/* Line number of the exception           */
 extern char	*_EXCEPT_File;			/* File name of the exception             */
 extern int	_EXCEPT_Errno;			/* Error number from most recent sys call */
 extern int (*_EXCEPT_Cleanup)();	/* Function to call to clean up (or NULL) */
+extern void _EXCEPT_( char *fmt, ... );
 
 #if defined(__cplusplus)
 }
