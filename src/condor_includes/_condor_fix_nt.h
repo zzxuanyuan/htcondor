6365d287a0ad3b247eeddc3dfe0a241dd479bcce
jbasney <jbasney>
1997-09-11 20:28:27 +0000

    use O_RDONLY and friends on NT

--- src/condor_includes/_condor_fix_nt.h
+++ src/condor_includes/_condor_fix_nt.h
@@ -2,12 +2,18 @@
 #define _CONDOR_FIX_NT
 
 #include <io.h>
+#include <fcntl.h>
 #define lseek _lseek
+#define O_RDONLY _O_RDONLY
+#define O_WRONLY _O_WRONLY
+#define O_RDWR _O_RDWR
+#define O_CREAT _O_CREAT
 #include <sys/stat.h>
 #define stat _stat
 #define MAXPATHLEN 1024
 #define MAXHOSTNAMELEN 64
 #define strcasecmp _stricmp
 #define strncasecmp _strnicmp
+#include <time.h>
 
 #endif
