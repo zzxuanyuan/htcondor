a054f69339cac85905aacfe04e3843d5eebc0eeb
wright <wright>
1997-10-06 19:13:36 +0000

    Use our own isalnum() function, since different libc's can get it wrong.

--- src/condor_util_lib/config.c
+++ src/condor_util_lib/config.c
@@ -59,9 +59,20 @@ char	*getline(), *expand_macro(), *ltrunc(), *lookup_macro();
 
 int		ConfigLineNo;
 
-#define ISIDCHAR(c)		(isalnum(c) || ((c) == '_'))
-#define ISOP(c)		(((c) == '=') || ((c) == ':'))
+int
+condor_isalnum(int c)
+{
+	if( ('a' <= c && c <= 'z') ||
+		('A' <= c && c <= 'Z') ||
+		('0' <= c && c <= '9') ) {
+		return 1;
+	} else {
+		return 0;
+	}
+}
 
+#define ISIDCHAR(c)		(condor_isalnum(c) || ((c) == '_'))
+#define ISOP(c)		(((c) == '=') || ((c) == ':'))
 
 
 /*
