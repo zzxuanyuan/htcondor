9186df273b7198f719ba20820fcbed5db9db32e6
colbster <colbster>
1999-03-18 00:15:46 +0000

    Re-Purging old code.

--- src/condor_collector.V6/instantiate.C
+++
@@ -1,7 +0,0 @@
-#include "condor_common.h"
-#include "MyString.h"
-#include "HashTable.h"
-#include "view_server.h"
-
-template class HashTable<MyString, GeneralRecord*>;
-template class HashBucket<MyString, GeneralRecord*>;
