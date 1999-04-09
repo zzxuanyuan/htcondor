ce42ce51d3e05b5adf38e63b2a29bb9c6d6398df
thain <thain>
1999-04-09 15:16:47 +0000

    This hack is now conditional on the definition of CONDOR_KILLS_STAT_DEFINITIONS.

--- src/condor_includes/condor_fix_sys_stat.h
+++ src/condor_includes/condor_fix_sys_stat.h
@@ -11,6 +11,9 @@ of stat and friends.
 
 The whole point of this is to enable us to redefine
 the libc versions of stat and friends as we please.
+We only perform this action when CONDOR_KILL_STAT_DEFINITIONS
+is in effect.  This should probably only be set
+by the syscall library.
 
 Notice that a #define stat is not what we want -- this
 will clobber all sorts of arguments and structures that
@@ -20,33 +23,37 @@ stat(), we will only kill the static function definition.
 
 #include "condor_header_features.h"
 
-#define mknod(a,b,c) __condor_hack_mknod(a,b,c)
-#define stat(a,b) __condor_hack_stat(a,b)
-#define lstat(a,b) __condor_hack_lstat(a,b)
-#define fstat(a,b) __condor_hack_fstat(a,b)
-#define __mknod(a,b,c) __condor_hack___mknod(a,b,c)
-#define __stat(a,b) __condor_hack___stat(a,b)
-#define __lstat(a,b) __condor_hack___lstat(a,b)
-#define __fstat(a,b) __condor_hack___fstat(a,b)
+#if defined(CONDOR_KILLS_STAT_DEFINITIONS)
+	#define mknod(a,b,c) __condor_hack_mknod(a,b,c)
+	#define stat(a,b) __condor_hack_stat(a,b)
+	#define lstat(a,b) __condor_hack_lstat(a,b)
+	#define fstat(a,b) __condor_hack_fstat(a,b)
+	#define __mknod(a,b,c) __condor_hack___mknod(a,b,c)
+	#define __stat(a,b) __condor_hack___stat(a,b)
+	#define __lstat(a,b) __condor_hack___lstat(a,b)
+	#define __fstat(a,b) __condor_hack___fstat(a,b)
+#endif
 
 #include <sys/stat.h>
 
-#undef fstat
-#undef lstat
-#undef stat
-#undef mknod
-#undef __fstat
-#undef __lstat
-#undef __stat
-#undef __mknod
+#if defined(CONDOR_KILLS_STAT_DEFINITIONS)
+	#undef fstat
+	#undef lstat
+	#undef stat
+	#undef mknod
+	#undef __fstat
+	#undef __lstat
+	#undef __stat
+	#undef __mknod
 
-/* Now, we must provide the protoypes that we lost. */
+	/* Now, we must provide the protoypes that we lost. */
 
-BEGIN_C_DECLS
-extern int stat(const char *path, struct stat *buf);
-extern int lstat(const char *path, struct stat *buf);
-extern int fstat(int fd, struct stat *buf);
-extern int mknod(const char *path, mode_t mode, dev_t device);
-END_C_DECLS
+	BEGIN_C_DECLS
+	extern int stat(const char *path, struct stat *buf);
+	extern int lstat(const char *path, struct stat *buf);
+	extern int fstat(int fd, struct stat *buf);
+	extern int mknod(const char *path, mode_t mode, dev_t device);
+	END_C_DECLS
+#endif
 
 #endif /* _CONDOR_FIX_SYS_STAT_H */
