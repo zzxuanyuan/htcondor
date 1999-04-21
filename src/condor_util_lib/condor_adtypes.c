9f1873911a59ad50ede824c8665738c7b27e9e16
colbster <colbster>
1999-04-21 21:29:02 +0000

    Added condor_adtypes.c

---
+++ src/condor_util_lib/condor_adtypes.c
@@ -0,0 +1,39 @@
+/* 
+** Copyright 1998 by the Condor Team
+** 
+** Permission to use, copy, modify, and distribute this software and its
+** documentation for any purpose and without fee is hereby granted,
+** provided that the above copyright notice appear in all copies and that
+** both that copyright notice and this permission notice appear in
+** supporting documentation, and that the names of the University of
+** Wisconsin and the Condor Design Team not be used in advertising or
+** publicity pertaining to distribution of the software without specific,
+** written prior permission.  The University of Wisconsin and the Condor
+** Design Team make no representations about the suitability of this
+** software for any purpose.  It is provided "as is" without express
+** or implied warranty.
+** 
+** THE UNIVERSITY OF WISCONSIN AND THE CONDOR DESIGN TEAM DISCLAIM ALL
+** WARRANTIES WITH REGARD TO THIS SOFTWARE, INCLUDING ALL IMPLIED WARRANTIES
+** OF MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE UNIVERSITY OF
+** WISCONSIN OR THE CONDOR DESIGN TEAM BE LIABLE FOR ANY SPECIAL, INDIRECT
+** OR CONSEQUENTIAL DAMAGES OR ANY DAMAGES WHATSOEVER RESULTING FROM LOSS
+** OF USE, DATA OR PROFITS, WHETHER IN AN ACTION OF CONTRACT, NEGLIGENCE
+** OR OTHER TORTIOUS ACTION, ARISING OUT OF OR IN CONNECTION WITH THE USE
+** OR PERFORMANCE OF THIS SOFTWARE.
+** 
+** 
+*/
+
+#include "condor_adtypes.h"
+
+const char STARTD_ADTYPE      [] = "Machine";
+const char SCHEDD_ADTYPE      [] = "Scheduler";
+const char MASTER_ADTYPE      [] = "DaemonMaster";
+const char CKPT_SRVR_ADTYPE   [] = "CkptServer";
+const char JOB_ADTYPE         [] = "Job";
+const char QUERY_ADTYPE       [] = "Query";
+const char COLLECTOR_ADTYPE   [] = "Collector";
+const char CKPT_FILE_ADTYPE   [] = "CkptFile";
+const char USERAUTH_ADTYPE    [] = "Authentication";
+const char LICENSE_ADTYPE     [] = "License";
