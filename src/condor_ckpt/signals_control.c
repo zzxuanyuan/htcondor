3789c71b6fe59c8bae2c55571a4ec25c7ddb4174
thain <thain>
1999-08-18 20:15:52 +0000

    Changed semantics of enable() and disable(). Now, this module keeps an internal count of the number of times diable() has been called.  The signal mask is no longer returned to the caller.

--- src/condor_ckpt/signals_control.c
+++ src/condor_ckpt/signals_control.c
@@ -1,38 +1,55 @@
-
 #include "signals_control.h"
 #include "condor_syscall_mode.h"
 #include "condor_debug.h"
 
-sigset_t _condor_signals_disable()
+static int disable_count = 0;
+
+void _condor_signals_disable()
 {
 	int sigscm;
-	sigset_t mask, omask;
+	sigset_t mask;
 
-	/* Block signals requesting an exit or checkpoint for duration of
+	disable_count++;
+
+       	/* Block signals requesting an exit or checkpoint for duration of
 	   system call. */
+
 	sigscm = SetSyscalls( SYS_LOCAL | SYS_UNMAPPED );
+
 	sigemptyset( &mask );
 	sigaddset( &mask, SIGTSTP );
 	sigaddset( &mask, SIGUSR1 );
 	sigaddset( &mask, SIGUSR2 );
-	if( sigprocmask(SIG_BLOCK,&mask,&omask) < 0 ) {
-		dprintf(D_ALWAYS, "sigprocmask failed: %s\n", strerror(errno));
-		return omask;
+
+	if( sigprocmask(SIG_BLOCK,&mask,0) < 0 ) {
+		dprintf(D_ALWAYS, "_condor_signals_disable: sigprocmask failed: %s\n", strerror(errno));
 	}
+
 	SetSyscalls( sigscm );
-	return omask;
 }
 
-void _condor_signals_enable(sigset_t omask)
+void _condor_signals_enable()
 {
 	int sigscm;
+	sigset_t mask;
 
-	sigscm = SetSyscalls( SYS_LOCAL | SYS_UNMAPPED );
-	/* Restore previous signal mask - generally unblocks TSTP and USR1 */
-	if( sigprocmask(SIG_SETMASK,&omask,0) < 0 ) {
-		dprintf(D_ALWAYS, "sigprocmask failed: %s\n", strerror(errno));
-		return;
+	disable_count--;
+
+	if(disable_count<=0) {
+		disable_count=0;
+
+		sigscm = SetSyscalls( SYS_LOCAL | SYS_UNMAPPED );
+
+		sigemptyset( &mask );
+		sigaddset( &mask, SIGTSTP );
+		sigaddset( &mask, SIGUSR1 );
+		sigaddset( &mask, SIGUSR2 );
+
+		if( sigprocmask(SIG_UNBLOCK,&mask,0) < 0 )
+			dprintf(D_ALWAYS, "_condor_signals_enable: sigprocmask failed: %s\n", strerror(errno));
+
+		SetSyscalls( sigscm );
 	}
-	SetSyscalls( sigscm );
 }	
+
 
--- src/condor_ckpt/signals_control.h
+++ src/condor_ckpt/signals_control.h
@@ -3,21 +3,19 @@
 #define _CONDOR_SIGNALS_CONTROL_H
 
 /*
-These two functions disable and enable the handling
-of the special Condor signals, regardless of the state of the
-signal mapping table.
-
-These functions were moved to the ckpt module becuase they functionally
-belong to the signal handling code, and serve to hide the hairy
-details of signal implementation.
+These two functions control whether asynchronous Condor events
+(checkpointing, suspension, etc.) are processed.  An internal
+count of the number of times disable() has been called is kept.
+To re-enable signals, enable() must be called once for every time
+disable() has been called.
 */
 
 #include "condor_common.h"
 
 BEGIN_C_DECLS
 
-sigset_t _condor_signals_disable();
-void     _condor_signals_enable(sigset_t s);
+void	_condor_signals_disable();
+void	_condor_signals_enable();
 
 END_C_DECLS
 
