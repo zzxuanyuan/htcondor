8e67a77d8dfa4784658b4efe9d870281a4dbd838
pfc <pfc>
2003-01-08 22:12:45 +0000

    added call to config() so param() will work

--- src/condor_dagman/t_helper.C
+++ src/condor_dagman/t_helper.C
@@ -2,10 +2,14 @@
 #include <string>
 #include <fstream>
 #include "helper.h"
+#include "condor_config.h"
 
 int main(int argc, char* argv[])
 {
   assert(argc == 2);
+
+  config();   // load Condor configuration info
+
   std::string output_file(Helper().resolve(argv[1]));
   std::ofstream output_file_created(output_file.c_str());
   assert(output_file_created);
