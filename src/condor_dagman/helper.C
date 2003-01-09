7ca66a3e1ffd2decdd2c509ab22b28dc54626436
pfc <pfc>
2003-01-09 17:26:43 +0000

    with Francesco's permission, replaced EDG license with Condor license

--- src/condor_dagman/helper.C
+++ src/condor_dagman/helper.C
@@ -1,9 +1,30 @@
-// File: Helper.cpp
-// Author: Francesco Giacomini <Francesco.Giacomini@cnaf.infn.it>
-// Copyright (c) 2002 EU DataGrid.
-// For license conditions see http://www.eu-datagrid.org/license.html
+/***************************Copyright-DO-NOT-REMOVE-THIS-LINE**
+ * CONDOR Copyright Notice
+ *
+ * See LICENSE.TXT for additional notices and disclaimers.
+ *
+ * Copyright (c)1990-2003 CONDOR Team, Computer Sciences Department, 
+ * University of Wisconsin-Madison, Madison, WI.  All Rights Reserved.  
+ * No use of the CONDOR Software Program Source Code is authorized 
+ * without the express consent of the CONDOR Team.  For more information 
+ * contact: CONDOR Team, Attention: Professor Miron Livny, 
+ * 7367 Computer Sciences, 1210 W. Dayton St., Madison, WI 53706-1685, 
+ * (608) 262-0856 or miron@cs.wisc.edu.
+ *
+ * U.S. Government Rights Restrictions: Use, duplication, or disclosure 
+ * by the U.S. Government is subject to restrictions as set forth in 
+ * subparagraph (c)(1)(ii) of The Rights in Technical Data and Computer 
+ * Software clause at DFARS 252.227-7013 or subparagraphs (c)(1) and 
+ * (2) of Commercial Computer Software-Restricted Rights at 48 CFR 
+ * 52.227-19, as applicable, CONDOR Team, Attention: Professor Miron 
+ * Livny, 7367 Computer Sciences, 1210 W. Dayton St., Madison, 
+ * WI 53706-1685, (608) 262-0856 or miron@cs.wisc.edu.
+ ****************************Copyright-DO-NOT-REMOVE-THIS-LINE**/
 
-// $Id: helper.C,v 1.1.2.2 2002-12-24 11:13:27 giaco Exp $
+// File: Helper.cpp
+// Author: Francesco Giacomini, INFN <Francesco.Giacomini@cnaf.infn.it>
+
+// $Id: helper.C,v 1.1.2.3 2003-01-09 17:26:43 pfc Exp $
 
 #include "helper.h"
 #include <fstream>
