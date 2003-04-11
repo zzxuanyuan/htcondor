bb062698fc092d69c0c82ca51aa497f97ebd80cb
unknown <unknown>
2003-04-11 14:12:34 +0000

    This commit was manufactured by cvs2svn to create tag 'HAWKEYE_V1_0_RC2'.

--- src/condor_negotiator.V6/matchmaker_instantiate.C
+++
@@ -1,5 +0,0 @@
-#include "HashTable.h"
-#include "MyString.h"
-#include "matchmaker.h"
-
-template class HashTable<MyString, MapEntry *>;
--- src/condor_startd.V6/CondorSystrayCommon.h
+++
@@ -1,8 +0,0 @@
-#ifndef CONDOR_SYSTRAY_COMMON_H
-#define CONDOR_SYSTRAY_COMMON_H
-
-const int CONDOR_SYSTRAY_NOTIFY_CHANGED = WM_USER + 4000;
-
-enum ECpuStatus { kSystrayStatusIdle, kSystrayStatusClaimed, kSystrayStatusJobRunning, kSystrayStatusJobSuspended, kSystrayStatusJobPreempting };
-
-#endif
