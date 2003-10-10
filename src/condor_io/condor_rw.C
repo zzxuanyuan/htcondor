4be9b6042e4874a04b44b5b784f25ab5d01bb913
tannenba <tannenba>
2003-10-10 09:23:21 +0000

    Added socket 'flags' parameter to condor_read() and condor_write(). Motivation: daemon_core wanted to use MSG_PEEK, but still honor cedar timeout values.

--- src/condor_io/condor_rw.C
+++ src/condor_io/condor_rw.C
@@ -55,7 +55,7 @@ static int errno_is_temporary( int e )
  * a timeout, and make sure that all data is read or written.
  */
 int
-condor_read( SOCKET fd, char *buf, int sz, int timeout )
+condor_read( SOCKET fd, char *buf, int sz, int timeout, int flags )
 {
 	int nr = 0, nro;
 	unsigned int start_time, cur_time;
@@ -116,7 +116,7 @@ condor_read( SOCKET fd, char *buf, int sz, int timeout )
 			}
 		}
 
-		nro = recv(fd, &buf[nr], sz - nr, 0);
+		nro = recv(fd, &buf[nr], sz - nr, flags);
 		if( nro <= 0 ) {
 			int the_error;
 #ifdef WIN32
@@ -146,7 +146,7 @@ condor_read( SOCKET fd, char *buf, int sz, int timeout )
 
 
 int
-condor_write( SOCKET fd, char *buf, int sz, int timeout )
+condor_write( SOCKET fd, char *buf, int sz, int timeout, int flags )
 {
 	int nw = 0, nwo = 0;
 	unsigned int start_time = 0, cur_time = 0;
@@ -256,7 +256,7 @@ condor_write( SOCKET fd, char *buf, int sz, int timeout )
 			}
 		}
 		
-		nwo = send(fd, &buf[nw], sz - nw, 0);
+		nwo = send(fd, &buf[nw], sz - nw, flags);
 
 		if( nwo <= 0 ) {
 			int the_error;
