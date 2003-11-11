87a54261574d21d344d43a1e54e63effd752621d
wright <wright>
2003-11-11 08:29:07 +0000

    merged V6_5-branch as tagged at V6_7-root into trunk-merge-V6_5-branch-1

--- src/condor_starter.V6.1/parallel_master_proc.h
+++ src/condor_starter.V6.1/parallel_master_proc.h
@@ -21,14 +21,33 @@
  * WI 53706-1685, (608) 262-0856 or miron@cs.wisc.edu.
 ****************************Copyright-DO-NOT-REMOVE-THIS-LINE**/
 
-#include "condor_common.h"
-#include "list.h"
-#include "HashTable.h"
-#include "MyString.h"
-#include "file_transfer.h"
-#include "user_proc.h"
+#ifndef _CONDOR_PARALLEL_MASTER_PROC
+#define _CONDOR_PARALLEL_MASTER_PROC
 
-template class List<UserProc>;
-template class HashBucket<MyString, FileTransfer *>;
-template class HashBucket<int, FileTransfer *>;
-template class Item<UserProc>;
+#include "condor_common.h"
+#include "condor_classad.h"
+#include "parallel_comrade_proc.h"
+
+/** This class is for the parallel "master" process.  It's derived from
+	the comrade class (it's "more equal" than the others :-) ).  It 
+	is special because it alters the path of the master before it's 
+	started, so that our sneaky rsh can get called instead of the
+	regular rsh.  Also, a variable is slipped into the environment for
+	our rsh so it knows how to contact the shadow... */
+
+class ParallelMasterProc : public ParallelComradeProc
+{
+ public:
+    
+    ParallelMasterProc( ClassAd * jobAd );
+    virtual ~ParallelMasterProc();
+
+    virtual int StartJob();
+
+ private:
+
+        /// puts sneaky rsh first in path; puts PARALLEL_SHADOW_SINFUL in env
+    int alterEnv();
+};
+
+#endif
