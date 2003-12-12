f9d02901f3078c04cbb2ed5060d9dfb5a7835977
wright <wright>
2003-12-12 07:02:14 +0000

    added ATTR_LAST_CONTACT to the list of attributes we always want to try to update to the job queue.

--- src/condor_shadow.V6.1/baseshadow.C
+++ src/condor_shadow.V6.1/baseshadow.C
@@ -338,6 +338,7 @@ BaseShadow::initJobQueueAttrLists( void )
 	common_job_queue_attrs->insert( ATTR_LAST_SUSPENSION_TIME );
 	common_job_queue_attrs->insert( ATTR_BYTES_SENT );
 	common_job_queue_attrs->insert( ATTR_BYTES_RECVD );
+	common_job_queue_attrs->insert( ATTR_LAST_CONTACT );
 
 	hold_job_queue_attrs = new StringList();
 	hold_job_queue_attrs->insert( ATTR_HOLD_REASON );
