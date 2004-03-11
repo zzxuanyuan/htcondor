7658f6afce4b5ef2550f0c156cecfbf534a185ee
jfrey <jfrey>
2004-03-11 00:56:55 +0000

    When the gridmanager deletes job ads from the schedd, do all the deletes in one large transaction rather than each in a seperate transaction.

--- src/condor_gridmanager/gridmanager.C
+++ src/condor_gridmanager/gridmanager.C
@@ -439,7 +439,6 @@ doContactSchedd()
 	bool schedd_deletes_complete = false;
 	bool add_remove_jobs_complete = false;
 	bool commit_transaction = true;
-	List<BaseJob> successful_deletes;
 	int failure_line_num = 0;
 	bool send_reschedule = false;
 
@@ -844,6 +843,13 @@ dprintf(D_FULLDEBUG,"***Trying job type %s\n",job_type->Name);
 
 	// Delete existing jobs
 	/////////////////////////////////////////////////////
+	BeginTransaction();
+	if ( errno == ETIMEDOUT ) {
+		failure_line_num = __LINE__;
+		commit_transaction = false;
+		goto contact_schedd_disconnect;
+	}
+
 	pendingScheddUpdates.startIterations();
 
 	while ( pendingScheddUpdates.iterate( curr_job ) != 0 ) {
@@ -851,12 +857,6 @@ dprintf(D_FULLDEBUG,"***Trying job type %s\n",job_type->Name);
 		if ( curr_job->deleteFromSchedd ) {
 			dprintf(D_FULLDEBUG,"Deleting job %d.%d from schedd\n",
 					curr_job->procID.cluster, curr_job->procID.proc);
-			BeginTransaction();
-			if ( errno == ETIMEDOUT ) {
-				failure_line_num = __LINE__;
-				commit_transaction = false;
-				goto contact_schedd_disconnect;
-			}
 			rc = DestroyProc(curr_job->procID.cluster,
 							 curr_job->procID.proc);
 			if ( rc < 0 ) {
@@ -864,14 +864,14 @@ dprintf(D_FULLDEBUG,"***Trying job type %s\n",job_type->Name);
 				commit_transaction = false;
 				goto contact_schedd_disconnect;
 			}
-			if ( CloseConnection() < 0 ) {
-				failure_line_num = __LINE__;
-				commit_transaction = false;
-				goto contact_schedd_disconnect;
-			}
-			successful_deletes.Append( curr_job );
 		}
 
+	}
+
+	if ( CloseConnection() < 0 ) {
+		failure_line_num = __LINE__;
+		commit_transaction = false;
+		goto contact_schedd_disconnect;
 	}
 
 	schedd_deletes_complete = true;
@@ -911,7 +911,7 @@ dprintf(D_FULLDEBUG,"***Trying job type %s\n",job_type->Name);
 				// object yet; wait until we successfully delete the job
 				// from the schedd.
 			if ( curr_job->deleteFromSchedd == true &&
-				 successful_deletes.Delete( curr_job ) == false ) {
+				 schedd_deletes_complete == false ) {
 				continue;
 			}
 
