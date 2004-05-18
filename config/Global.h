b8be75fb949d8b31212e85c16a9dee2d5ee97a54
psilord <psilord>
2004-05-18 18:39:10 +0000

    + Removed the previous macros for the concatenation macros and replaced them 	with macros which are tunable depending upon the ANSI specification of the 	cpp with respect to the ## operator.

--- config/Global.h
+++ config/Global.h
@@ -6,20 +6,51 @@
 #define YES	1
 #define NO	0
 
-#define __CONCAT(a,b) a ## b
-#define concat(a,b) __CONCAT(a,b)
+/* Concat - concatenates two strings.  */
+#ifndef Concat
+#if (__STDC__ && !defined(UnixCpp)) || defined(AnsiCpp)
+#define Concat(a,b)a##b
+#else
+#define Concat(a,b)a/**/b
+#endif
+#endif
 
-#define __CONCAT3(a,b,c) a ## b ## c
-#define concat3(a,b,c) __CONCAT3(a,b,c)
+/* Concat3 - concatenates three strings.  */
+#ifndef Concat3
+#if (__STDC__ && !defined(UnixCpp)) || defined(AnsiCpp)
+#define Concat3(a,b,c)a##b##c
+#else
+#define Concat3(a,b,c)a/**/b/**/c
+#endif
+#endif
 
-#define __CONCAT4(a,b,c,d) a ## b ## c ## d
-#define concat4(a,b,c,d) __CONCAT4(a,b,c,d)
+/* Concat4 - concatenates four strings.  */
+#ifndef Concat4
+#if (__STDC__ && !defined(UnixCpp)) || defined(AnsiCpp)
+#define Concat4(a,b,c,d)a##b##c##d
+#else
+#define Concat4(a,b,c,d)a/**/b/**/c/**/d
+#endif
+#endif
 
-#define __CONCAT5(a,b,c,d,e) a ## b ## c ## d ## e
-#define concat5(a,b,c,d,e) __CONCAT5(a,b,c,d,e)
+/* Concat5 - concatenates five strings.  */
+#ifndef Concat5
+#if (__STDC__ && !defined(UnixCpp)) || defined(AnsiCpp)
+#define Concat5(a,b,c,d,e)a##b##c##d##e
+#else
+#define Concat5(a,b,c,d,e)a/**/b/**/c/**/d/**/e
+#endif
+#endif
 
-#define __CONCAT6(a,b,c,d,e,f) a ## b ## c ## d ## e ## f
-#define concat6(a,b,c,d,e,f) __CONCAT6(a,b,c,d,e,f)
+/* Concat6 - concatenates six strings.  */
+#ifndef Concat6
+#if (__STDC__ && !defined(UnixCpp)) || defined(AnsiCpp)
+#define Concat6(a,b,c,d,e,f)a##b##c##d##e##f
+#else
+#define Concat6(a,b,c,d,e,f)a/**/b/**/c/**/d/**/e/**/f
+#endif
+#endif
+
 
 
 /***************************************************************************
