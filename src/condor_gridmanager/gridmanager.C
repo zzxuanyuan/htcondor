1af55dc5992049913a3b161bd49e8ec118aebc32
jfrey <jfrey>
2004-06-01 04:36:21 +0000

    Added Unicore to jobTypes list in the gridmanager.

--- src/condor_gridmanager/gridmanager.C
+++ src/condor_gridmanager/gridmanager.C
@@ -46,6 +46,7 @@
 #include "nordugridjob.h"
 #endif
 
+#include "unicorejob.h"
 #include "mirrorjob.h"
 #include "gt3job.h"
 
@@ -329,6 +330,15 @@ Init()
 	new_type->CreateFunc = NordugridJobCreate;
 	jobTypes.Append( new_type );
 #endif
+
+	new_type = new JobType;
+	new_type->Name = strdup( "Unicore" );
+	new_type->InitFunc = UnicoreJobInit;
+	new_type->ReconfigFunc = UnicoreJobReconfig;
+	new_type->AdMatchConst = UnicoreJobAdConst;
+	new_type->AdMustExpandFunc = UnicoreJobAdMustExpand;
+	new_type->CreateFunc = UnicoreJobCreate;
+	jobTypes.Append( new_type );
 
 	new_type = new JobType;
 	new_type->Name = strdup( "Mirror" );
