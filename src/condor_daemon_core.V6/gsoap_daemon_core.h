500ec8b8a5c0cc6e02fcdd120164173d19b3554c
matt <matt>
2004-06-09 16:00:39 +0000

    Fixed a foolish bug, the 'encoding-style' should be 'encoded'!

--- src/condor_daemon_core.V6/gsoap_daemon_core.h
+++ src/condor_daemon_core.V6/gsoap_daemon_core.h
@@ -1,7 +1,7 @@
 #import "gsoap_daemon_core_types.h"
 
 //gsoap condor service name: condor
-//gsoap condor service encoding: encoding-style
+//gsoap condor service encoding: encoded
 
 int condor__getInfoAd(void *_, struct condor__ClassAdStructAndStatus & ad);
 int condor__getVersionString(void *_, struct condor__StringAndStatus & verstring);
