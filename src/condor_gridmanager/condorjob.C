cb88bd81c96e4608645238fe9bf8e338f4486a2b
jfrey <jfrey>
2004-07-08 16:40:20 +0000

    When building the job ad to submit to the remote schedd, start with the local job ad and exclude/change attributes we know shouldn't be copied across. With the old method, we started with an empty ad and only copied attributes we knew we needed from the local ad. Now, user-defined attributes will be included.

--- src/condor_gridmanager/condorjob.C
+++ src/condor_gridmanager/condorjob.C
@@ -1014,6 +1014,8 @@ BaseResource *CondorJob::GetResource()
 	return (BaseResource *)NULL;
 }
 
+#if 0
+// Old white-list version
 ClassAd *CondorJob::buildSubmitAd()
 {
 	int now = time(NULL);
@@ -1109,6 +1111,116 @@ ClassAd *CondorJob::buildSubmitAd()
 
 	return submit_ad;
 }
+#else
+// New black-list version
+ClassAd *CondorJob::buildSubmitAd()
+{
+	int now = time(NULL);
+	MyString expr;
+	ClassAd *submit_ad;
+	ExprTree *next_expr;
+
+		// Base the submit ad on our own job ad
+	submit_ad = new ClassAd( *ad );
+
+	submit_ad->Delete( ATTR_CLUSTER_ID );
+	submit_ad->Delete( ATTR_PROC_ID );
+	submit_ad->Delete( ATTR_REMOTE_SCHEDD );
+	submit_ad->Delete( ATTR_REMOTE_POOL );
+	submit_ad->Delete( ATTR_JOB_MATCHED );
+	submit_ad->Delete( ATTR_JOB_MANAGED );
+	submit_ad->Delete( ATTR_MIRROR_ACTIVE );
+	submit_ad->Delete( ATTR_MIRROR_JOB_ID );
+	submit_ad->Delete( ATTR_MIRROR_LEASE_TIME );
+	submit_ad->Delete( ATTR_MIRROR_RELEASED );
+	submit_ad->Delete( ATTR_MIRROR_REMOTE_LEASE_TIME );
+	submit_ad->Delete( ATTR_MIRROR_SCHEDD );
+	submit_ad->Delete( ATTR_STAGE_IN_FINISH );
+	submit_ad->Delete( ATTR_STAGE_IN_START );
+	submit_ad->Delete( ATTR_SCHEDD_BIRTHDATE );
+	submit_ad->Delete( ATTR_X509_USER_PROXY );
+	submit_ad->Delete( ATTR_X509_USER_PROXY_SUBJECT );
+	submit_ad->Delete( ATTR_FILE_SYSTEM_DOMAIN );
+	submit_ad->Delete( ATTR_ULOG_FILE );
+	submit_ad->Delete( ATTR_ULOG_USE_XML );
+	submit_ad->Delete( ATTR_NOTIFY_USER );
+	submit_ad->Delete( ATTR_ON_EXIT_HOLD_CHECK );
+	submit_ad->Delete( ATTR_ON_EXIT_REMOVE_CHECK );
+	submit_ad->Delete( ATTR_PERIODIC_HOLD_CHECK );
+	submit_ad->Delete( ATTR_PERIODIC_RELEASE_CHECK );
+	submit_ad->Delete( ATTR_PERIODIC_REMOVE_CHECK );
+	submit_ad->Delete( ATTR_SERVER_TIME );
+	submit_ad->Delete( ATTR_JOB_MANAGED );
+	submit_ad->Delete( ATTR_GLOBAL_JOB_ID );
+	submit_ad->Delete( "CondorPlatform" );
+	submit_ad->Delete( "CondorVersion" );
+	submit_ad->Delete( ATTR_JOB_GRID_TYPE );
+	submit_ad->Delete( ATTR_WANT_CLAIMING );
+	submit_ad->Delete( ATTR_WANT_MATCHING );
+	submit_ad->Delete( ATTR_HOLD_REASON );
+	submit_ad->Delete( ATTR_HOLD_REASON_CODE );
+	submit_ad->Delete( ATTR_HOLD_REASON_SUBCODE );
+	submit_ad->Delete( ATTR_LAST_HOLD_REASON );
+	submit_ad->Delete( ATTR_LAST_HOLD_REASON_CODE );
+	submit_ad->Delete( ATTR_LAST_HOLD_REASON_SUBCODE );
+	submit_ad->Delete( ATTR_RELEASE_REASON );
+	submit_ad->Delete( ATTR_LAST_RELEASE_REASON );
+	submit_ad->Delete( ATTR_JOB_STATUS_ON_RELEASE );
+
+	submit_ad->Assign( ATTR_JOB_STATUS, HELD );
+	submit_ad->Assign( ATTR_HOLD_REASON, "Spooling input data files" );
+	submit_ad->Assign( ATTR_JOB_UNIVERSE, CONDOR_UNIVERSE_VANILLA );
+
+	submit_ad->Assign( ATTR_Q_DATE, now );
+	submit_ad->Assign( ATTR_CURRENT_HOSTS, 0 );
+	submit_ad->Assign( ATTR_COMPLETION_DATE, 0 );
+	submit_ad->Assign( ATTR_JOB_REMOTE_WALL_CLOCK, (float)0.0 );
+	submit_ad->Assign( ATTR_JOB_LOCAL_USER_CPU, (float)0.0 );
+	submit_ad->Assign( ATTR_JOB_LOCAL_SYS_CPU, (float)0.0 );
+	submit_ad->Assign( ATTR_JOB_REMOTE_USER_CPU, (float)0.0 );
+	submit_ad->Assign( ATTR_JOB_REMOTE_SYS_CPU, (float)0.0 );
+	submit_ad->Assign( ATTR_JOB_EXIT_STATUS, 0 );
+	submit_ad->Assign( ATTR_NUM_CKPTS, 0 );
+	submit_ad->Assign( ATTR_NUM_RESTARTS, 0 );
+	submit_ad->Assign( ATTR_NUM_SYSTEM_HOLDS, 0 );
+	submit_ad->Assign( ATTR_JOB_COMMITTED_TIME, 0 );
+	submit_ad->Assign( ATTR_TOTAL_SUSPENSIONS, 0 );
+	submit_ad->Assign( ATTR_LAST_SUSPENSION_TIME, 0 );
+	submit_ad->Assign( ATTR_CUMULATIVE_SUSPENSION_TIME, 0 );
+	submit_ad->Assign( ATTR_ON_EXIT_BY_SIGNAL, false );
+	submit_ad->Assign( ATTR_ENTERED_CURRENT_STATUS, now  );
+	submit_ad->Assign( ATTR_JOB_NOTIFICATION, NOTIFY_NEVER );
+
+	expr.sprintf( "%s = (%s > 0) =!= True && CurrentTime > %s + %d",
+				  ATTR_PERIODIC_REMOVE_CHECK, ATTR_STAGE_IN_FINISH,
+				  ATTR_Q_DATE, 1800 );
+	submit_ad->Insert( expr.Value() );
+
+	expr.sprintf( "%s = %s == %d", ATTR_JOB_LEAVE_IN_QUEUE, ATTR_JOB_STATUS,
+				  COMPLETED );
+	submit_ad->Insert( expr.Value() );
+
+	submit_ad->Assign( ATTR_SUBMITTER_ID, submitterId );
+
+	ad->ResetExpr();
+	while ( (next_expr = ad->NextExpr()) != NULL ) {
+		if ( strncmp( ((Variable*)next_expr->LArg())->Name(), "REMOTE_", 7 ) == 0 ) {
+			char *attr_value;
+			MyString buf;
+			submit_ad->Delete( ((Variable*)next_expr->LArg())->Name() );
+			next_expr->RArg()->PrintToNewStr(&attr_value);
+			buf.sprintf( "%s = %s", &((Variable*)next_expr->LArg())->Name()[7],
+						 attr_value );
+			submit_ad->Insert( buf.Value() );
+			free(attr_value);
+		}
+	}
+
+		// worry about ATTR_JOB_[OUTPUT|ERROR]_ORIG
+
+	return submit_ad;
+}
+#endif
 
 ClassAd *CondorJob::buildStageInAd()
 {
