c4a9819745cb2934631e3f8d02b13b6dee224258
jhuang <jhuang>
2004-11-09 23:16:50 +0000

    don't include odbc.h here

--- src/condor_daemon_core.V6/condor_daemon_core.h
+++ src/condor_daemon_core.V6/condor_daemon_core.h
@@ -51,8 +51,8 @@
 #ifdef WIN32
 #include "ntsysinfo.h"
 #endif
-#include "pgsqldatabase.h"
-#include "odbc.h"
+//#include "pgsqldatabase.h"
+//#include "odbc.h"
 
 #define DEBUG_SETTABLE_ATTR_LISTS 0
 
@@ -1201,6 +1201,6 @@ extern DaemonCore* daemonCore;
 char *global_dc_sinful( void );
 
 //extern PGSQLDatabase *DBObj;
-extern ODBC *DBObj;
+//extern ODBC *DBObj;
 
 #endif /* _CONDOR_DAEMON_CORE_H_ */
