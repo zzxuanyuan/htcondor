614b841c1a4e3c4f993a9e25b7bf153a28b80c69
srinath <srinath>
2005-02-14 18:50:41 +0000

    Added timeout Added reconnect in case of error

--- src/condor_c++_util/odbc.C
+++ src/condor_c++_util/odbc.C
@@ -18,14 +18,17 @@ ODBC::ODBC()
 	strcpy(dsn,"");
 	strcpy(user,"");
 	strcpy(auth,"");
+	timeout = 0;
+	in_xact = false;
 }
-
-ODBC::ODBC(char *dsn, char *user, char *auth)
+ODBC::ODBC(char *dsn, char *user, char *auth, char *timeout)
 {
 	connected = false;
 	strncpy(this->dsn,dsn,80);
 	strncpy(this->user,user,30);
 	strncpy(this->auth,auth,80);
+	this->timeout = atoi(timeout);
+	in_xact = false;
 }
 
 ODBC::~ODBC()
@@ -45,7 +48,6 @@ long ODBC::odbc_connect()
 
 	if(isConnected())
 		odbc_disconnect();
-
 	odbc_retcode=SQLAllocHandle(SQL_HANDLE_ENV,SQL_NULL_HANDLE,&odbc_env);
 	if ((odbc_retcode != SQL_SUCCESS) && (odbc_retcode != SQL_SUCCESS_WITH_INFO))
 	{
@@ -105,7 +107,15 @@ long ODBC::odbc_connect()
 
 	dprintf(D_FULLDEBUG,"Connected %s:%s:%s:\n",dsn,user,auth);
 	connected = true;
-	return odbc_retcode;	
+
+	char timeout_stmt[50];
+	sprintf(timeout_stmt, "set statement_timeout= %d", timeout);
+
+	odbc_sqlstmt(timeout_stmt);
+
+	return odbc_retcode;
+	
+	
 }
 
 long ODBC::odbc_connect(char *dsn, char *user, char *auth)
@@ -127,23 +137,92 @@ long ODBC::odbc_disconnect()
 	dprintf(D_FULLDEBUG,"Disconnected\n");
 	return(0);
 }
+long ODBC::odbc_beginxtstmt(const char * statement)
+{
+	int odbc_retcode;
+	dprintf(D_FULLDEBUG,"%s Begin\n",statement);
+	if(!connected)
+	{
+		if(in_xact)
+			return SQL_ERROR;
+		else
+		{
+			odbc_retcode = odbc_connect();
+			if((odbc_retcode != SQL_SUCCESS) && (odbc_retcode != SQL_SUCCESS_WITH_INFO)) 
+			{
+				in_xact = false;
+				dprintf(D_FULLDEBUG,"Failed to connect -- skipping begin statement\n");
+				return odbc_retcode;
+			}	
+		}
+	}
+	odbc_retcode=SQLExecDirect( odbc_hstmt,(SQLCHAR*)statement,SQL_NTS);
+	if ((odbc_retcode != SQL_SUCCESS) && (odbc_retcode != SQL_SUCCESS_WITH_INFO))
+	{
+		dprintf(D_FULLDEBUG,"Error in Statement %d\n",odbc_retcode);
+		SQLGetDiagRec(SQL_HANDLE_DBC, odbc_hdbc,1, (SQLCHAR*)odbc_stat,&odbc_err,(SQLCHAR*)odbc_msg,100,&odbc_mlen);
+		dprintf(D_FULLDEBUG,"%s (%d)\n",odbc_msg,odbc_err);
+		odbc_disconnect();
+		in_xact = false;
+	}
+	else // XXX We go to in_xact only if the begin statement succeeds. Is this what we want ?
+		in_xact = true; 
+	return odbc_retcode;
 
+}
+long ODBC::odbc_endxtstmt(const char * statement)
+{
+	int odbc_retcode;
+	dprintf(D_FULLDEBUG,"%s End\n",statement);
+	if(!connected)
+	{
+		//  XXX We connect anyway and let the DB decide what to do
+		odbc_retcode = odbc_connect();
+		if((odbc_retcode != SQL_SUCCESS) && (odbc_retcode != SQL_SUCCESS_WITH_INFO)) 
+		{
+			in_xact = false;
+			dprintf(D_FULLDEBUG,"Failed to connect -- skipping end statement\n");
+			return odbc_retcode;
+		}	
+	}
+	odbc_retcode=SQLExecDirect( odbc_hstmt,(SQLCHAR*)statement,SQL_NTS);
+	if ((odbc_retcode != SQL_SUCCESS) && (odbc_retcode != SQL_SUCCESS_WITH_INFO))
+	{
+		dprintf(D_FULLDEBUG,"Error in Statement %d\n",odbc_retcode);
+		SQLGetDiagRec(SQL_HANDLE_DBC, odbc_hdbc,1, (SQLCHAR*)odbc_stat,&odbc_err,(SQLCHAR*)odbc_msg,100,&odbc_mlen);
+		dprintf(D_FULLDEBUG,"%s (%d)\n",odbc_msg,odbc_err);
+		odbc_disconnect();
+	}
+	in_xact = false; 
+	return odbc_retcode;
+
+}
 long ODBC::odbc_sqlstmt(const char* statement)
 {
 	int odbc_retcode;
-
-	if (!connected) {
-		dprintf(D_FULLDEBUG,"odbc_sqlstmt: not connected yet\n");
-		return -1;
+	dprintf(D_FULLDEBUG,"%s sqlstmt \n",statement,connected);
+	if(!connected)
+	{
+		if(in_xact)
+			return SQL_ERROR;
+		else
+		{
+			odbc_retcode = odbc_connect();
+			if((odbc_retcode != SQL_SUCCESS) && (odbc_retcode != SQL_SUCCESS_WITH_INFO)) 
+			{
+				in_xact = false;
+				dprintf(D_FULLDEBUG,"Failed to connect -- skipping sql statement\n");
+				return odbc_retcode;
+			}	
+		}
 	}
-
-	dprintf(D_FULLDEBUG,"%s\n",statement);
 	odbc_retcode=SQLExecDirect( odbc_hstmt,(SQLCHAR*)statement,SQL_NTS);
 	if ((odbc_retcode != SQL_SUCCESS) && (odbc_retcode != SQL_SUCCESS_WITH_INFO))
 	{
 		dprintf(D_FULLDEBUG,"Error in Statement %d\n",odbc_retcode);
 		SQLGetDiagRec(SQL_HANDLE_DBC, odbc_hdbc,1, (SQLCHAR*)odbc_stat,&odbc_err,(SQLCHAR*)odbc_msg,100,&odbc_mlen);
 		dprintf(D_FULLDEBUG,"odbc_msg:%s (odbc_err=%d)\n",odbc_msg,odbc_err);
+		odbc_disconnect();
 	}
 	return odbc_retcode;
 }
@@ -152,7 +231,7 @@ long ODBC::odbc_bindcol(unsigned short col_number, void *result, int buffer_len,
 {
 	if (!connected) {
 		dprintf(D_FULLDEBUG,"odbc_bindcol: not connected yet\n");
-		return -1;
+		return SQL_ERROR;
 	}
 	
 	return 	(SQLBindCol(odbc_hstmt, col_number, col_type, result,(SQLINTEGER)buffer_len, &odbc_err));
@@ -162,7 +241,7 @@ long ODBC::odbc_fetch()
 {
 	if (!connected) {
 		dprintf(D_FULLDEBUG,"odbc_fetch: not connected yet\n");
-		return -1;
+		return SQL_ERROR;
 	}
 	
 	return (SQLFetch(odbc_hstmt));
@@ -173,7 +252,7 @@ ODBC *createConnection() {
 	ODBC *ptr;
 	int odbc_retcode;
 
-	char *tmp, *odbc_dsn, *odbc_user, *odbc_auth;
+	char *tmp, *odbc_dsn, *odbc_user, *odbc_auth, *odbc_timeout;
 	
 	/* Parse ODBC Connection Params */
 	tmp = param("ODBC_DSN");
@@ -203,16 +282,25 @@ ODBC *createConnection() {
 		odbc_auth = strdup("");
 	}
 
-	ptr = new ODBC(odbc_dsn, odbc_user, odbc_auth);
-		
+	tmp = param("ODBC_TIMEOUT");
+	if( tmp ) {
+		odbc_timeout = strdup(tmp);
+		free(tmp);
+	}
+	else {
+		odbc_timeout = strdup("300");
+	}
+
+	ptr = new ODBC(odbc_dsn, odbc_user, odbc_auth, odbc_timeout);
+        free(odbc_dsn);
+	free(odbc_user);
+	free(odbc_auth);
+        free(odbc_timeout);
+
 	odbc_retcode = ptr->odbc_connect();
 	if ((odbc_retcode != SQL_SUCCESS) && (odbc_retcode != SQL_SUCCESS_WITH_INFO)) {
 		dprintf(D_FULLDEBUG, "createConnection: connection failed\n");
 	}
-
-	free(odbc_dsn);
-	free(odbc_user);
-	free(odbc_auth);
 
 	return ptr;
 }
