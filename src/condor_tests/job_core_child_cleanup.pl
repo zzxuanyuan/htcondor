a9f6e91e154ead6cb3847bd020a810b40fa17f6f
bt <bt>
2005-06-02 19:10:15 +0000

    Made changes to the new test to see how well we clean up child processes. We now spawn a number of processes which each spawn a process on their own. This version has 20 processes spawned 10 forks with 1 fork from each.

--- src/condor_tests/job_core_child_cleanup.pl
+++ src/condor_tests/job_core_child_cleanup.pl
@@ -15,24 +15,62 @@ $path = getcwd();
 my $pout = "$path/parent";
 my $cout = "$path/child";
 my $count;
+my $innercount = 0;
+my $innerpid = 0;
 
 # parent shouldn't wait for any children
 $SIG{'CHLD'} = "IGNORE";
 
-open(POUT, "> job_core_child_cleanup.data") or die "Could not open file '$pout': $?";
+open(POUT, "> job_core_child_cleanup.data") or die "Could not open file 'job_core_child_cleanup.data': $?";
 print POUT "Parent's pid is $$\n\n";
 
+system("rm -rf job_core_child_cleanup.data.kids");
+
 $count = 0;
-while ($count < 10) {
+while ($count < 10) 
+{
 	$pid = fork();
+	$innercount = 0;
 	if ($pid == 0)
 	{
 		# child code....
 
+		open(GCPOUT, ">> job_core_child_cleanup.data.kids") or die "Could not open file 'job_core_child_cleanup.data.kids': $?";
 		# child waits until a signal shows up
 		$SIG{'INT'} = \&handler;
 		$SIG{'HUP'} = \&handler;
 		$SIG{'TERM'} = \&handler;
+
+		while ($innercount < 1) 
+		{
+
+			$innerpid = fork();
+			if ($innerpid == 0)
+			{
+				# child code....
+		
+				# child waits until a signal shows up
+				$SIG{'INT'} = \&handler;
+				$SIG{'HUP'} = \&handler;
+				$SIG{'TERM'} = \&handler;
+	
+				while (1) { sleep 1; }
+	
+				exit 1;
+			}
+
+			# parent code...
+	
+			if (!defined($innerpid)) {
+				print GCPOUT "Grand kid fork failed!!!!!!!!!!!!!!!!!!!\n";
+				die "some problem forking. Oh well.\n";
+			}
+
+			print GCPOUT "Relationship: $$ child created $innerpid\n";
+			close(GCPOUT);
+	
+			$innercount++;
+		}
 
 		while (1) { sleep 1; }
 
