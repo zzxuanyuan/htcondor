ac4cc606775da7745b2c38128b08e14b3f2111e4
nleroy <nleroy>
2005-10-04 21:01:00 +0000

    Added a DC for the "match maker lite"

---
+++ src/condor_daemon_client/dc_match_maker.C
@@ -0,0 +1,291 @@
+/***************************Copyright-DO-NOT-REMOVE-THIS-LINE**
+  *
+  * Condor Software Copyright Notice
+  * Copyright (C) 1990-2004, Condor Team, Computer Sciences Department,
+  * University of Wisconsin-Madison, WI.
+  *
+  * This source code is covered by the Condor Public License, which can
+  * be found in the accompanying LICENSE.TXT file, or online at
+  * www.condorproject.org.
+  *
+  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+  * AND THE UNIVERSITY OF WISCONSIN-MADISON "AS IS" AND ANY EXPRESS OR
+  * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+  * WARRANTIES OF MERCHANTABILITY, OF SATISFACTORY QUALITY, AND FITNESS
+  * FOR A PARTICULAR PURPOSE OR USE ARE DISCLAIMED. THE COPYRIGHT
+  * HOLDERS AND CONTRIBUTORS AND THE UNIVERSITY OF WISCONSIN-MADISON
+  * MAKE NO MAKE NO REPRESENTATION THAT THE SOFTWARE, MODIFICATIONS,
+  * ENHANCEMENTS OR DERIVATIVE WORKS THEREOF, WILL NOT INFRINGE ANY
+  * PATENT, COPYRIGHT, TRADEMARK, TRADE SECRET OR OTHER PROPRIETARY
+  * RIGHT.
+  *
+  ****************************Copyright-DO-NOT-REMOVE-THIS-LINE**/
+
+#include "condor_common.h"
+#include "condor_debug.h"
+#include "condor_config.h"
+#include "condor_string.h"
+#include "condor_ver_info.h"
+#include "condor_attributes.h"
+
+#include "daemon.h"
+#include "dc_match_lite.h"
+#include "internet.h"
+
+#define WANT_NAMESPACES
+#include "classad_distribution.h"
+#include "newclassad_stream.h"
+using namespace std;
+
+// *** DCMatchLiteLease (class to hold lease informat)  class methods ***
+
+DCMatchLiteLease::DCMatchLiteLease( void )
+{
+	lease_ad = NULL;
+	lease_duration = 0;
+	release_lease_when_done = true;
+}
+
+DCMatchLiteLease::DCMatchLiteLease(
+	classad::ClassAd		*ad
+	)
+{
+	lease_ad = NULL;
+	initFromClassAd( ad );
+}
+
+DCMatchLiteLease::DCMatchLiteLease(
+	const classad::ClassAd	&ad
+	)
+{
+	lease_ad = NULL;
+	initFromClassAd( ad );
+}
+
+DCMatchLiteLease::DCMatchLiteLease(
+	const string		&lease_id,
+	int					lease_duration )
+{
+	lease_ad = NULL;
+	setLeaseId( lease_id );
+	setLeaseDuration( lease_duration );
+	release_lease_when_done = true;
+}
+
+int
+DCMatchLiteLease::initFromClassAd(
+	const classad::ClassAd	&ad
+	)
+{
+	return initFromClassAd( new classad::ClassAd( ad ) );
+}
+
+int
+DCMatchLiteLease::initFromClassAd(
+	classad::ClassAd	*ad
+	)
+{
+	int		status = 0;
+	if ( lease_ad && (lease_ad != ad ) ) {
+		delete lease_ad;
+		lease_ad = NULL;
+	}
+	if ( !ad ) {
+		return 0;
+	}
+
+	lease_ad = ad;
+	if ( !lease_ad->EvaluateAttrString( "LeaseId", lease_id ) ) {
+		status = 1;
+		lease_id = "";
+	}
+	if ( !lease_ad->EvaluateAttrInt( "LeaseDuration", lease_duration ) ) {
+		status = 1;
+		lease_duration = 0;
+	}
+	if ( !lease_ad->EvaluateAttrBool( "ReleaseWhenDone",
+									  release_lease_when_done ) ) {
+		status = 1;
+		release_lease_when_done = true;
+	}
+	return status;
+}
+
+int
+DCMatchLiteLease::setLeaseId(
+	const string		&lease_id )
+{
+	this->lease_id = lease_id;
+	return 0;
+}
+
+int
+DCMatchLiteLease::setLeaseDuration(
+	int					duration )
+{
+	this->lease_duration = duration;
+	return 0;
+}
+
+void DCMatchLiteLease_FreeList( list<DCMatchLiteLease *> &lease_list )
+{
+	for( list<DCMatchLiteLease *>::iterator iter = lease_list.begin();
+		 iter != lease_list.end();
+		 iter++ ) {
+		delete *iter;
+	}
+}
+
+
+// *** The main DCMatchLite class methods ***
+
+DCMatchLite::DCMatchLite( const char* name, const char *pool )
+		: Daemon( DT_MATCH_LITE, name, pool )
+{
+}
+
+
+DCMatchLite::~DCMatchLite( void )
+{
+}
+
+
+bool
+DCMatchLite::getMatches( const classad::ClassAd &request_ad,
+						 list< DCMatchLiteLease *> &leases )
+{
+		// Create the ReliSock
+	ReliSock *rsock = (ReliSock *)startCommand(
+			MATCHLITE_GET_MATCH, Stream::reli_sock, 20 );
+	if ( ! rsock ) {
+		return false;
+	}
+
+		// Serialize the classad onto the wire
+	if ( !StreamPut( rsock, request_ad ) ) {
+		delete rsock;
+		return false;
+	}
+
+	rsock->eom();
+
+		// Receive the return code
+	rsock->decode();
+
+	int		num_matches;
+	if ( !rsock->code( num_matches ) ) {
+		delete rsock;
+		return false;
+	}
+	if ( num_matches < 0 ) {
+		rsock->close( );
+		delete rsock;
+		return true;
+	}
+	for (int num = 0;  num < num_matches;  num++ ) {
+		classad::ClassAd	*ad = new classad::ClassAd( );
+		if ( !StreamGet( rsock, *ad ) ) {
+			delete rsock;
+			delete ad;
+			return false;
+		}
+		DCMatchLiteLease *lease = new DCMatchLiteLease( ad );
+		leases.push_back( lease );
+	}
+
+	rsock->close();
+	return true;
+}
+
+bool
+DCMatchLite::getMatches( const char *requestor_name,
+						 int number_requested,
+						 int duration,
+						 const char *requirements,
+						 const char *rank,
+						 list< DCMatchLiteLease *> &leases )
+{
+	if ( ( ! requestor_name ) ||
+		 ( number_requested < 0 ) ||
+		 ( duration < 0 ) ) {
+		return false;
+	}
+
+	classad::ClassAd	ad;
+	ad.InsertAttr( "Name", requestor_name );
+	ad.InsertAttr( "RequestCount", number_requested );
+	ad.InsertAttr( "LeaseDuration", duration );
+	if ( requirements ) {
+		ad.InsertAttr( "Requirements", requirements );
+	}
+	if ( rank ) {
+		ad.InsertAttr( "Rank", rank );
+	}
+
+	return getMatches( ad, leases );
+}
+
+bool
+DCMatchLite::renewLeases(
+	const list< const DCMatchLiteLease *> &leases,
+	list< DCMatchLiteLease *> &out_leases )
+{
+		// Create the ReliSock
+	ReliSock *rsock = (ReliSock *)startCommand(
+			MATCHLITE_GET_MATCH, Stream::reli_sock, 20 );
+	if ( ! rsock ) {
+		return false;
+	}
+
+	// Send the number of 
+
+	// Send the requests
+	for ( list<const DCMatchLiteLease *>::iterator iter = leases.begin();
+		  iter != leases.end();
+		  iter++ )
+	{
+		
+	}
+		// Serialize the classad onto the wire
+	if ( !StreamPut( rsock, request_ad ) ) {
+		delete rsock;
+		return false;
+	}
+
+	rsock->eom();
+
+		// Receive the return code
+	rsock->decode();
+
+	int		num_matches;
+	if ( !rsock->code( num_matches ) ) {
+		delete rsock;
+		return false;
+	}
+	if ( num_matches < 0 ) {
+		rsock->close( );
+		delete rsock;
+		return true;
+	}
+	for (int num = 0;  num < num_matches;  num++ ) {
+		char	*lease_id;
+		int		duration;
+		bool	release_when_done;
+
+		DCMatchLiteLease	*lease = readLease( rsock );
+		if ( !lease ) {
+			delete rsock;
+		}
+		classad::ClassAd	*ad = new classad::ClassAd( );
+		if ( !StreamGet( rsock, *ad ) ) {
+			delete rsock;
+			delete ad;
+			return false;
+		}
+		DCMatchLiteLease *lease = new DCMatchLiteLease( ad );
+		leases.push_back( lease );
+	}
+
+	rsock->close();
+	return true;
+}
