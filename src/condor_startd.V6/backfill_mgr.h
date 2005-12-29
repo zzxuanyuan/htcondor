17bf959b019e9e1735bbd9d69b3d47c4798ab0a1
wright <wright>
2005-12-29 17:39:21 +0000

    make the BackfillMgr derived from the DaemonCore Service class so we can register class members as DC handlers

--- src/condor_startd.V6/backfill_mgr.h
+++ src/condor_startd.V6/backfill_mgr.h
@@ -24,6 +24,7 @@
 #define _CONDOR_BACKFILL_MGR_H
 
 #include "condor_common.h"
+#include "../condor_daemon_core.V6/condor_daemon_core.h"
 #include "condor_classad.h"
 #include "condor_state.h"
 #include "extArray.h"
@@ -81,7 +82,7 @@ protected:
    derived classes for BOINC, but someday, others might be added.
 */
 
-class BackfillMgr
+class BackfillMgr : public Service
 {
 public:
 	BackfillMgr(); 
