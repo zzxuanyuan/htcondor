c4464b80720d809160b562cc3feceb72ec804600
jmeehean <jmeehean>
2006-10-17 21:23:55 +0000

    Updated the version tag for this new branch.

--- src/condor_c++_util/condor_version.C
+++ src/condor_c++_util/condor_version.C
@@ -33,7 +33,7 @@
    will EXCEPT at startup time.  
 */
 
-static char* CondorVersionString = "$CondorVersion: 6.8.2 " __DATE__ " $";
+static char* CondorVersionString = "$CondorVersion: 6.8.2 " __DATE__ " V6_8_2-ENVAWARE-BRANCH $";
 
 /* 
    This is some wisdom from Cygnus's web page.  If you just try to use
