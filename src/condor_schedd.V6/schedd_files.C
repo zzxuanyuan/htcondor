f5d8cbb144f9a30b248b88d3261edb88a62f8bf9
jhuang <jhuang>
2007-01-22 20:02:37 +0000

    change string type MyString to avoid memory freeing problem

--- src/condor_schedd.V6/schedd_files.C
+++ src/condor_schedd.V6/schedd_files.C
@@ -7,77 +7,71 @@
 #include "basename.h"
 #include "my_hostname.h"
 #include "file_sql.h"
+#include "quill_enums.h"
 
 extern FILESQL *FILEObj;
 
 #define MAXSQLLEN 500
 #define TIMELEN 30
 
-int schedd_files_ins_file(
-						  char *fileName,
-						  char *fs_domain,
-						  char *path,
+QuillErrCode schedd_files_ins_file(
+						  const char *fileName,
+						  const char *fs_domain,
+						  const char *path,
 						  time_t f_ts,
 						  int fsize)
 {
-	int retcode;
 	ClassAd tmpCl1;
 	ClassAd *tmpClP1 = &tmpCl1;
-	char tmp[512];
+	MyString tmp;
 
-	snprintf(tmp, 512, "f_name = \"%s\"", fileName);
-	tmpClP1->Insert(tmp);		
+	tmp.sprintf("f_name = \"%s\"", fileName);
+	tmpClP1->Insert(tmp.GetCStr());		
 
-	snprintf(tmp, 512, "f_host = \"%s\"", fs_domain);
-	tmpClP1->Insert(tmp);		
+	tmp.sprintf("f_host = \"%s\"", fs_domain);
+	tmpClP1->Insert(tmp.GetCStr());	
 
-	snprintf(tmp, 512, "f_path = \"%s\"", path);
-	tmpClP1->Insert(tmp);		
+	tmp.sprintf("f_path = \"%s\"", path);
+	tmpClP1->Insert(tmp.GetCStr());	
 
-	snprintf(tmp, 512, "f_ts = %d", (int)f_ts);
-	tmpClP1->Insert(tmp);
+	tmp.sprintf("f_ts = %d", (int)f_ts);
+	tmpClP1->Insert(tmp.GetCStr());	
 		
-	snprintf(tmp, 512, "f_size = %d", fsize);
-	tmpClP1->Insert(tmp);
+	tmp.sprintf("f_size = %d", fsize);
+	tmpClP1->Insert(tmp.GetCStr());	
 
-	if (FILEObj->file_newEvent("Files", tmpClP1) == FAILURE) {
-		retcode = -1;
-	}
-	else {
-		retcode = 0;
-	}
-	return retcode;
+	return FILEObj->file_newEvent("Files", tmpClP1);
 }
 
 void schedd_files_ins_usage(
-							char *globalJobId,
+							const char *globalJobId,
 							const char *type,
-							char *fileName,
-							char *fs_domain,
-							char *path,
+							const char *fileName,
+							const char *fs_domain,
+							const char *path,
 							time_t f_ts)
 {
 	ClassAd tmpCl1;
 	ClassAd *tmpClP1 = &tmpCl1;
-	char tmp[512];
+	MyString tmp;
 
-	snprintf(tmp, 512, "f_name = \"%s\"", fileName);
-	tmpClP1->Insert(tmp);		
+	tmp.sprintf("f_name = \"%s\"", fileName);
+	tmpClP1->Insert(tmp.GetCStr());	
 
-	snprintf(tmp, 512, "f_host = \"%s\"", fs_domain);
-	tmpClP1->Insert(tmp);		
+	tmp.sprintf("f_host = \"%s\"", fs_domain);
+	tmpClP1->Insert(tmp.GetCStr());	
 
-	snprintf(tmp, 512, "f_path = \"%s\"", path);
-	tmpClP1->Insert(tmp);		
+	tmp.sprintf("f_path = \"%s\"", path);
+	tmpClP1->Insert(tmp.GetCStr());	
 
-	snprintf(tmp, 512, "f_ts = %d", (int) f_ts);
-	tmpClP1->Insert(tmp);
+	tmp.sprintf("f_ts = %d", (int) f_ts);
+	tmpClP1->Insert(tmp.GetCStr());	
 
-	snprintf(tmp, 512, "globalJobId = \"%s\"", globalJobId);
-	tmpClP1->Insert(tmp);
+	tmp.sprintf("globalJobId = \"%s\"", globalJobId);
+	tmpClP1->Insert(tmp.GetCStr());	
 	
-	snprintf(tmp, 512, "type = \"%s\"", type);
-	tmpClP1->Insert(tmp);
+	tmp.sprintf("type = \"%s\"", type);
+	tmpClP1->Insert(tmp.GetCStr());	
 
 	FILEObj->file_newEvent("Fileusages", tmpClP1);
 }
@@ -86,83 +80,73 @@ void schedd_files_ins(
 					  ClassAd *procad, 
 					  const char *type)
 {
-	char *fileName = NULL,
-		*tmpFile = NULL,
-		*fs_domain = NULL,    /* submitter's file system domain */
-		*path = NULL,
-		*globalJobId = NULL,
-		*tmp;
+	MyString fileName = "", tmpFile = "", fs_domain = "", path = "", 
+		globalJobId = "";
 
-	char *pathname = (char *)malloc(_POSIX_PATH_MAX);
+	MyString pathname = "";
 
 	struct stat file_status;
-	bool freeFsDomain = FALSE;
 	bool fileExist = TRUE;
-	int len;
-	int retcode;
+	QuillErrCode retcode;
 
 		// get the file name (possibly with path) from classad
-	procad->LookupString(type, &tmpFile);
+	procad->LookupString(type, tmpFile);
 
 		// get host machine name from classad
-	if (!procad->LookupString(ATTR_FILE_SYSTEM_DOMAIN, &fs_domain)) {
+	if (!procad->LookupString(ATTR_FILE_SYSTEM_DOMAIN, fs_domain)) {
 			// use my hostname for files if fs_domain not found
 		fs_domain = my_full_hostname();
-	} else
-		freeFsDomain = TRUE;
+	}
 	
 		// get current working directory
-	procad->LookupString(ATTR_JOB_IWD, &path);
+	procad->LookupString(ATTR_JOB_IWD, path);
 
-	procad->LookupString(ATTR_GLOBAL_JOB_ID, &globalJobId);
+	procad->LookupString(ATTR_GLOBAL_JOB_ID, globalJobId);
 
 		// determine file name, path.
-	if (fullpath(tmpFile)) {
-		if (strcmp(tmpFile, "/dev/null") == 0)
+	if (fullpath(tmpFile.GetCStr())) {
+		if (strcmp(tmpFile.GetCStr(), "/dev/null") == 0)
 			return; /* job doesn't care about this type of file */
 		
-		strcpy(pathname, tmpFile);
+		pathname = tmpFile;
 		
-		free(path);	// free prev path first
-		path = dirname(tmpFile);
-		fileName = basename(tmpFile); 
+		path = dirname(tmpFile.GetCStr());
+		fileName = basename(tmpFile.GetCStr()); 
 	}
 	else {
-		sprintf(pathname, "%s/%s", path, tmpFile);
+		pathname.sprintf("%s/%s", path.GetCStr(), tmpFile.GetCStr());
 		fileName = tmpFile;
 	}
 
 		// get the file status which contains timestamp and size
-	if (stat(pathname, &file_status) < 0) {
+	if (stat(pathname.GetCStr(), &file_status) < 0) {
 		dprintf(D_FULLDEBUG, "ERROR: File '%s' can not be accessed.\n", 
-				pathname);
+				pathname.GetCStr());
 		fileExist = FALSE;
 	}
 
 	if(!fileExist) {
-		goto schedd_files_ins_end;
+		return;
 	}
 
 		// insert the file entry into the files table
-	retcode = schedd_files_ins_file(fileName, fs_domain, path, 
+	retcode = schedd_files_ins_file(fileName.GetCStr(), 
+									fs_domain.GetCStr(), 
+									path.GetCStr(), 
 									file_status.st_mtime, 
 									file_status.st_size);
 	
-	if (retcode < 0) {
+	if (retcode == FAILURE) {
 			// fail to insert the file
-		goto schedd_files_ins_end;
+		return;
 	}
 
 		// insert a usage for this file by this job
-	schedd_files_ins_usage(globalJobId, type, fileName, fs_domain, path, 
+	schedd_files_ins_usage(globalJobId.GetCStr(), type, 
+						   fileName.GetCStr(), 
+						   fs_domain.GetCStr(), 
+						   path.GetCStr(), 
 						   file_status.st_mtime);
-
-schedd_files_ins_end:
-	if (path) free(path);
-	if (pathname) free(pathname);
-	if (globalJobId) free(globalJobId);
-	if (tmpFile) free(tmpFile);
-	if (freeFsDomain) free(fs_domain);
 }
 
 void schedd_files(ClassAd *procad)
