d24e7e307c34a3a646698dcbfcdf9c2636fc8e12
unknown <unknown>
2007-02-24 01:20:01 +0000

    This commit was manufactured by cvs2svn to create branch 'unlabeled-1.83.2'.

--- src/condor_transferd/td_instantiate.C
+++
@@ -1,8 +0,0 @@
-#include "condor_common.h"
-#include "../condor_daemon_core.V6/condor_daemon_core.h"
-#include "condor_classad.h"
-#include "list.h"
-#include "condor_td.h"
-
-template class HashTable<MyString, TransferRequest*>;
-template class HashTable<long, TransferRequest*>;
