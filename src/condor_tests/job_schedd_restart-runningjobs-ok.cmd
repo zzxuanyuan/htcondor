67b02c2e55f90d12cc19d33d6c9fb8288f4778f9
unknown <unknown>
2007-06-06 20:50:52 +0000

    This commit was manufactured by cvs2svn to create branch 'V6_9_3-branch-test'.

--- src/condor_tests/job_schedd_restart-runningjobs-ok.cmd
+++
@@ -1,8 +0,0 @@
-universe   = vanilla
-executable = x_sleep.pl
-output = job_schedd_restart-running-ok.out
-error = job_schedd_restart-running-ok.err
-log = job_schedd_restart-running-ok.log
-Notification = NEVER
-arguments = 0
-queue 10
