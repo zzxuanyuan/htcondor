6fccba6622aed46910c86fac2bb68cfc9fd0fbcf
alderman <alderman>
2007-07-24 12:35:36 +0000

    Changes reflect changes to signed ClassAd implementation; the new implementation (using gsoap's smdevp) is much simpler than the previous implementation (using gsoap's wsse).  So now this is back to what it was before I started on this branch.

--- src/condor_submit.V6/gsoap_submit.h
+++ src/condor_submit.V6/gsoap_submit.h
@@ -1,48 +1,29 @@
-#import "wsse.h"	// wsse = <http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd>
-//gsoap condor schema namespace:	urn:condor
-//gsoap condor schema form:	unqualified
-// Imported element "http://docs.oasis-open.org/wss/2004/01/oasis-200401-wss-wssecurity-secext-1.0.xsd":Security declared as _wsse__Security
+/***************************Copyright-DO-NOT-REMOVE-THIS-LINE**
+  *
+  * Condor Software Copyright Notice
+  * Copyright (C) 1990-2006, Condor Team, Computer Sciences Department,
+  * University of Wisconsin-Madison, WI.
+  *
+  * This source code is covered by the Condor Public License, which can
+  * be found in the accompanying LICENSE.TXT file, or online at
+  * www.condorproject.org.
+  *
+  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+  * AND THE UNIVERSITY OF WISCONSIN-MADISON "AS IS" AND ANY EXPRESS OR
+  * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+  * WARRANTIES OF MERCHANTABILITY, OF SATISFACTORY QUALITY, AND FITNESS
+  * FOR A PARTICULAR PURPOSE OR USE ARE DISCLAIMED. THE COPYRIGHT
+  * HOLDERS AND CONTRIBUTORS AND THE UNIVERSITY OF WISCONSIN-MADISON
+  * MAKE NO MAKE NO REPRESENTATION THAT THE SOFTWARE, MODIFICATIONS,
+  * ENHANCEMENTS OR DERIVATIVE WORKS THEREOF, WILL NOT INFRINGE ANY
+  * PATENT, COPYRIGHT, TRADEMARK, TRADE SECRET OR OTHER PROPRIETARY
+  * RIGHT.
+  *
+  ****************************Copyright-DO-NOT-REMOVE-THIS-LINE**/
+//gsoap condor service name: condorSubmit
+//gsoap condor service style: rpc
+//gsoap condor service encoding: encoded
+//gsoap condor service namespace: urn:condor
 
-#import "gsoap_daemon_core_types.h"
-/// "urn:condor":submitRequest is a complexType.
-struct _condor__signatureRequest
-{
-/// Element classAd of type "urn:condor":ClassAdStruct.
-    struct condor__ClassAdStruct*        classAd                        0;	///< Nullable pointer.
-};
+#import "gsoap_daemon_core.h"
 
-
-/// Element "urn:condor":submitResponse of complexType.
-
-/// "urn:condor":submitResponse is a complexType.
-struct _condor__signatureResponse
-{
-/// Element response of type xs:int.
-    int                                  response                       1;	///< Required element.
-};
-
-//gsoap condor service name:	condorSubmit
-//gsoap condor service type:	condorSubmitPortType 
-
-//gsoap condor service namespace:	urn:condor 
-//gsoap condor service transport:	http://schemas.xmlsoap.org/soap/http 
-
-struct SOAP_ENV__Header
-{
-    mustUnderstand                       // must be understood by receiver
-    _wsse__Security                     *wsse__Security                ;	///< TODO: Check element type (imported type)
-
-};
-
-//gsoap condor service method-style:	condorSubmit document
-//gsoap condor service method-encoding:	condorSubmit literal
-//gsoap condor service method-action:	condorSubmit ""
-//gsoap condor service method-input-header-part:	condorSubmit wsse__Security
-//gsoap condor service method-output-header-part:	condorSubmit wsse__Security
-int __condor__signature(
-    struct _condor__signatureRequest*      condor__signatureRequest,
-	void rv
-    //struct _condor__signatureResponse*     condor__signatureResponse ///< response parameter
-);
-
-/* End of successor.h */
