774cd5ee9dca75c0777e3c4e6c0f816684c09797
alderman <alderman>
2007-07-24 12:31:15 +0000

    Verify signed ClassAds.

---
+++ src/condor_starter.V6.1/verify_signed_classad.C
@@ -0,0 +1,38 @@
+/***************************Copyright-DO-NOT-REMOVE-THIS-LINE**
+  *
+  * Condor Software Copyright Notice
+  * Copyright (C) 1990-2006, Condor Team, Computer Sciences Department,
+  * University of Wisconsin-Madison, WI.
+  *
+  * This source code is covered by the Condor Public License, which can
+  * be found in the accompanying LICENSE.TXT file, or online at
+  * www.condorproject.org.
+  *
+  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+  * AND THE UNIVERSITY OF WISCONSIN-MADISON "AS IS" AND ANY EXPRESS OR
+  * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+  * WARRANTIES OF MERCHANTABILITY, OF SATISFACTORY QUALITY, AND FITNESS
+  * FOR A PARTICULAR PURPOSE OR USE ARE DISCLAIMED. THE COPYRIGHT
+  * HOLDERS AND CONTRIBUTORS AND THE UNIVERSITY OF WISCONSIN-MADISON
+  * MAKE NO MAKE NO REPRESENTATION THAT THE SOFTWARE, MODIFICATIONS,
+  * ENHANCEMENTS OR DERIVATIVE WORKS THEREOF, WILL NOT INFRINGE ANY
+  * PATENT, COPYRIGHT, TRADEMARK, TRADE SECRET OR OTHER PROPRIETARY
+  * RIGHT.
+  *
+  ****************************Copyright-DO-NOT-REMOVE-THIS-LINE**/
+
+#include "soap_starterH.h"
+#include "smdevp.h"
+#include "condor_common.h"
+#include "condor_classad.h"
+#include "condor_classad_util.h"
+#include "condor_attributes.h"
+#include "condor_debug.h"
+#include "condor_io.h"
+#include "condor_string.h"
+#include "list.h"
+
+#include <sys/mman.h>
+#include <sys/types.h>
+
+#include "../condor_c++_util/soap_signature_helpers.cpp"
