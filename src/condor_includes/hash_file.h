d6adf18e50bec567036eed005117911eeaf61960
alderman <alderman>
2007-08-24 21:19:18 +0000

    Added GSoap's smdevp.h for signed classads. Added hash_file.h for checksumming files.

---
+++ src/condor_includes/hash_file.h
@@ -0,0 +1,41 @@
+/***************************Copyright-DO-NOT-REMOVE-THIS-LINE**
+  *
+  * Condor Software Copyright Notice
+  * Copyright (C) 1990-2006, Condor Team, Computer Sciences Department,
+  * University of Wisconsin-Madison, WI.
+  *
+  * This source code is covered by the Condor Public License, which can
+  * be found in the accompanying LICENSE.TXT file, or online at
+  * www.condorproject.org.
+  *
+  * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+  * AND THE UNIVERSITY OF WISCONSIN-MADISON "AS IS" AND ANY EXPRESS OR
+  * IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED
+  * WARRANTIES OF MERCHANTABILITY, OF SATISFACTORY QUALITY, AND FITNESS
+  * FOR A PARTICULAR PURPOSE OR USE ARE DISCLAIMED. THE COPYRIGHT
+  * HOLDERS AND CONTRIBUTORS AND THE UNIVERSITY OF WISCONSIN-MADISON
+  * MAKE NO MAKE NO REPRESENTATION THAT THE SOFTWARE, MODIFICATIONS,
+  * ENHANCEMENTS OR DERIVATIVE WORKS THEREOF, WILL NOT INFRINGE ANY
+  * PATENT, COPYRIGHT, TRADEMARK, TRADE SECRET OR OTHER PROPRIETARY
+  * RIGHT.
+  *
+  ****************************Copyright-DO-NOT-REMOVE-THIS-LINE**/
+
+#ifndef __HASH_FILE_H
+#define __HASH_FILE_H
+
+#if defined(__cplusplus)
+extern "C" {
+#endif
+
+/* Given a filename, malloc and return a hex string of the sha256
+ * checksum of the file.  Caller responsible for freeing the result.
+ * Returns null in case of error.
+ */
+char *condor_hash_file(char *fn);
+
+#if defined(__cplusplus)
+}
+#endif
+
+#endif
