a7356473c450e5a24feaf1b2b1282d1e6c9225f9
burnett <burnett>
2007-10-02 14:53:13 +0000

    + Working command-line build update

--- src/condor_util_lib/uuid_lib.WIN32.c
+++ src/condor_util_lib/uuid_lib.WIN32.c
@@ -48,12 +48,14 @@ const IID IID_IDispatch = { 132096, 0, 0, {0xc0, 0, 0, 0, 0, 0, 0, 0x46} };
 //const IID IID_IPersistFile = { 267, 0, 0, {0xc0, 0, 0, 0, 0, 0, 0, 0x46} };
 const IID IID_IClassFactory = { 1, 0, 0, {0xc0, 0, 0, 0, 0, 0, 0, 0x46} };
 
-const IID IID_IDropTarget = { 290, 0, 0, {0xc0, 0, 0, 0, 0, 0, 0, 0x46} };
+//const IID IID_IDropTarget = { 290, 0, 0, {0xc0, 0, 0, 0, 0, 0, 0, 0x46} };
 //const IID IID_IStream = { 12, 0, 0, {0xc0, 0, 0, 0, 0, 0, 0, 0x46} };
 const IID IID_IUnknown = { 0, 0, 0, {0xc0, 0, 0, 0, 0, 0, 0, 0x46} };
 
 #if _MSC_VER == 1200
 // only on VC6
 const IID IID_IPersistFile = { 267, 0, 0, {0xc0, 0, 0, 0, 0, 0, 0, 0x46} };
+
+const IID IID_IDropTarget = { 290, 0, 0, {0xc0, 0, 0, 0, 0, 0, 0, 0x46} };
 const IID IID_IStream = { 12, 0, 0, {0xc0, 0, 0, 0, 0, 0, 0, 0x46} };
 #endif
