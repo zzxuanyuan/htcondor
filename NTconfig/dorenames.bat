f347848df4eafd34017894198c638a22eefd72a4
Ben Burnett <burnett@cs.wisc.edu>
2008-11-11 15:14:58 -0600

    Started getting rid of the scary dorenames.bat script.

--- NTconfig/dorenames.bat
+++ NTconfig/dorenames.bat
@@ -3,9 +3,9 @@ if exist ..\src\condor_master.V6\daemon.cpp goto happywindows
 echo Making happy for the repository...
 move ..\src\condor_master.V6\daemon_master.cpp ..\src\condor_master.V6\daemon.cpp
 move ..\src\condor_starter.V6.1\starter_class.cpp ..\src\condor_starter.V6.1\starter.cpp
-move ..\src\condor_util_lib\condor_common_c.c ..\src\condor_util_lib\condor_common.c
+REM move ..\src\condor_util_lib\condor_common_c.c ..\src\condor_util_lib\condor_common.c
 move ..\src\condor_c++_util\email_cpp.cpp ..\src\condor_c++_util\email.cpp
-move ..\src\condor_mail\condor_email_main.cpp ..\src\condor_mail\main.cpp
+REM move ..\src\condor_mail\condor_email_main.cpp ..\src\condor_mail\main.cpp
 move ..\src\condor_eventd\eventd_main.cpp ..\src\condor_eventd\main.cpp
 move ..\src\condor_dagman\dagman_submit.cpp ..\src\condor_dagman\submit.cpp
 move ..\src\condor_dagman\dagman_util.cpp ..\src\condor_dagman\util.cpp
@@ -15,9 +15,9 @@ exit /B 1
 echo Making happy for the Windows build...
 move ..\src\condor_master.V6\daemon.cpp ..\src\condor_master.V6\daemon_master.cpp
 move ..\src\condor_starter.V6.1\starter.cpp ..\src\condor_starter.V6.1\starter_class.cpp
-move ..\src\condor_util_lib\condor_common.c ..\src\condor_util_lib\condor_common_c.c
+REM move ..\src\condor_util_lib\condor_common.c ..\src\condor_util_lib\condor_common_c.c
 move ..\src\condor_c++_util\email.cpp ..\src\condor_c++_util\email_cpp.cpp
-move ..\src\condor_mail\main.cpp ..\src\condor_mail\condor_email_main.cpp
+REM move ..\src\condor_mail\main.cpp ..\src\condor_mail\condor_email_main.cpp
 move ..\src\condor_eventd\main.cpp ..\src\condor_eventd\eventd_main.cpp
 move ..\src\condor_dagman\submit.cpp ..\src\condor_dagman\dagman_submit.cpp
 move ..\src\condor_dagman\util.cpp ..\src\condor_dagman\dagman_util.cpp
