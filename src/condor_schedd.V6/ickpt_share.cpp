16fe479cc81dbe52fad1834e0a07b38e62829ebb
Greg Quinn <gquinn@cs.wisc.edu>
2009-01-30 13:02:23 -0600

    Add Windows support for the ickpt sharing stuff
    GitTrac #31

--- src/condor_schedd.V6/ickpt_share.cpp
+++ src/condor_schedd.V6/ickpt_share.cpp
@@ -36,6 +36,78 @@
 
 extern char* Spool;
 
+#if defined(WIN32)
+// a wrapper for CreateHardLink() on Windows that looks like link()
+// for the code in this module
+//
+static int
+ickpt_share_link(const char* oldpath, const char* newpath)
+{
+	if (CreateHardLink(newpath, oldpath, NULL)) {
+		return 0;
+	}
+	else {
+		DWORD error = GetLastError();
+		if (error == ERROR_FILE_NOT_FOUND) {
+			errno = ENOENT;
+		}
+		else {
+			// for anything but the ENOENT-equivalent, we'll
+			// log the error and set errno to something generic-
+			// sounding ("operation not permitted")
+			dprintf(D_ALWAYS,
+			        "CreateHardLink error: %u\n",
+			        (unsigned)error);
+			errno = EPERM;
+		}
+		return -1;
+	}
+}
+
+// stat on Windows  seems to always return 1 for link count,
+// so we need to provide our own (that only sets link
+// count, since that's all the code below cares about)
+//
+static int
+ickpt_share_stat(const char* path, struct _fixed_windows_stat* st)
+{
+	HANDLE handle = CreateFile(path,
+	                           GENERIC_READ,
+	                           FILE_SHARE_READ |
+	                               FILE_SHARE_WRITE |
+	                               FILE_SHARE_DELETE,
+	                           NULL,
+	                           OPEN_EXISTING,
+	                           0,
+	                           NULL);
+	if (handle == INVALID_HANDLE_VALUE) {
+		dprintf(D_ALWAYS,
+		        "ickpt_share: CreateFile error: %u\n",
+		        (unsigned)GetLastError());
+		errno = EPERM;
+		return -1;
+	}
+	BY_HANDLE_FILE_INFORMATION bhfi;
+	BOOL ret = GetFileInformationByHandle(handle, &bhfi);
+	DWORD err = GetLastError();
+	CloseHandle(handle);
+	if (!ret) {
+		dprintf(D_ALWAYS,
+		        "ickpt_share: GetFileInformationByHandle error: %u\n",
+		        (unsigned)err);
+		errno = EPERM;
+		return -1;
+	}
+	st->st_nlink = (short)bhfi.nNumberOfLinks;
+	return 0;
+}
+#else
+// UNIX: just use regular old link(2) and stat(2)
+//
+#define ickpt_share_link link
+#define ickpt_share_stat stat
+#endif
+
 // escape a string to that it is acceptable for use as a filename. any
 // character not in the regex [a-zA-Z._] is replaced with %AA, where AA
 // is the character's two-hex-digit equivalent
@@ -89,7 +161,7 @@ ickpt_share_try_sharing(const std::string& owner,
                         const std::string& ickpt_file)
 {
 	std::string hash_file = make_hash_filename(owner, hash);
-	if (link(hash_file.c_str(), ickpt_file.c_str()) == -1) {
+	if (ickpt_share_link(hash_file.c_str(), ickpt_file.c_str()) == -1) {
 		if (errno == ENOENT) {
 			dprintf(D_FULLDEBUG,
 			        "ickpt_share: %s not available for link from %s\n",
@@ -118,7 +190,7 @@ ickpt_share_init_sharing(const std::string& owner,
                          const std::string& ickpt_file)
 {
 	std::string hash_file = make_hash_filename(owner, hash);
-	if (link(ickpt_file.c_str(), hash_file.c_str()) == -1) {
+	if (ickpt_share_link(ickpt_file.c_str(), hash_file.c_str()) == -1) {
 		dprintf(D_ALWAYS,
 		        "ickpt_share: unexpected error linking %s to %s: %s\n",
 		        hash_file.c_str(),
@@ -137,7 +209,7 @@ ickpt_share_try_removal(const std::string& owner, const std::string& hash)
 {
 	std::string hash_file = make_hash_filename(owner, hash);
 	struct stat st;
-	if (stat(hash_file.c_str(), &st) == -1) {
+	if (ickpt_share_stat(hash_file.c_str(), &st) == -1) {
 		dprintf(D_ALWAYS,
 		        "ickpt_share: unexpected stat error on %s: %s\n",
 		        hash_file.c_str(),
