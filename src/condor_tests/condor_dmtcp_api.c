4db35dfaee1b291c59e6fb683306884b7bd3a3c4
Peter Keller <psilord@cs.wisc.edu>
2009-12-02 16:27:09 -0600

    Improved the build rules slightly for DMTCP. Fixed our condor checkpointing API wrapper to be more complete for fortran and whatnot. This is for ticket #366.

--- src/condor_tests/condor_dmtcp_api.c
+++ src/condor_tests/condor_dmtcp_api.c
@@ -1,11 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "dmtcpaware.h"
 
+#ifdef __cplusplus
+extern "C" {
+#endif
+
+/* C entry point */
 void ckpt_and_exit(void)
 {
-	
+	if (dmtcpCheckpoint () == DMTCP_AFTER_CHECKPOINT) {
+		exit (EXIT_SUCCESS);
+	}
 }
 
+/* fortran entry point */
+void ckpt_and_exit_(void)
+{
+	ckpt_and_exit();
+}
+
+/* fortran entry point */
+void ckpt_and_exit__(void)
+{
+	ckpt_and_exit();
+}
+
+/* C entry point */
 void ckpt(void)
 {
-
+	dmtcpCheckpoint();
 }
 
+/* fortran entry point */
+void ckpt_(void)
+{
+	ckpt();
+}
+
+/* fortran entry point */
+void ckpt__(void)
+{
+	ckpt();
+}
+
+#ifdef __cplusplus
+}
+#endif
