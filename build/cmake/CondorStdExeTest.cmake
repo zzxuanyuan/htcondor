5a34df01c139ec3e10336e0d35adb38e4693fbcb
Timothy St. Clair <tstclair@redhat.com>
2010-11-18 23:46:52 -0600

    Ticket #1248: Cleanup of install and test targets.  Updates are still required for std:u test targets

--- build/cmake/CondorStdExeTest.cmake
+++ build/cmake/CondorStdExeTest.cmake
@@ -26,10 +26,11 @@ MACRO (CONDOR_STD_EXE_TEST _CNDR_TARGET _COMPILER _SRCS _LINK_FLAGS )
 		command_target( cc_${_CNDR_TARGET} ${CONDOR_COMPILE} "-condor_lib;${STDU_LIB_LOC};-condor_ld_dir;${STDU_LIB_LOC};${_COMPILER};-o;${_CNDR_TARGET}.cndr.exe;lib${_CNDR_TARGET}.a;${_LINK_FLAGS}" "${_CNDR_TARGET}.cndr.exe" )
 		add_dependencies( cc_${_CNDR_TARGET} ${_CNDR_TARGET})
 
-		command_target( ca_${_CNDR_TARGET} ${CONDOR_ARCH_LINK} "${_CNDR_TARGET}.cndr.exe" )
+		command_target( ca_${_CNDR_TARGET} ${CONDOR_ARCH_LINK} "${_CNDR_TARGET}.cndr.exe" "${_CNDR_TARGET}.cndr.exe.Linux.${SYS_ARCH}")
 		add_dependencies( ca_${_CNDR_TARGET} cc_${_CNDR_TARGET})
 
-		append_var(CONDOR_TESTS ca_${_CNDR_TARGET})
+		#append_var(CONDOR_TESTS ca_${_CNDR_TARGET})
+		set_target_properties( cc_${_CNDR_TARGET} ca_${_CNDR_TARGET} PROPERTIES EXCLUDE_FROM_ALL TRUE)
 
 	endif()
 
