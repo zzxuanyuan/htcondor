67a838becc247716f47e7b12ddaf9756c62557d9
Timothy St. Clair <tstclair@redhat.com>
2010-11-19 13:58:43 -0600

    Ticket #1248: Missed in last commit

--- build/cmake/CondorStdExeTest.cmake
+++ build/cmake/CondorStdExeTest.cmake
@@ -20,17 +20,30 @@ MACRO (CONDOR_STD_EXE_TEST _CNDR_TARGET _COMPILER _SRCS _LINK_FLAGS )
 
 	if (STD_UNIVERSE AND BUILD_TESTS)
 
+		# so normally you should be able to pass the archive directly to 
+		foreach(_file ${_SRCS})
+			if(objs_${_CNDR_TARGET})
+				set(objs_${_CNDR_TARGET} ${_file}.o)
+			else()
+				 set(objs_${_CNDR_TARGET} ${objs_${_CNDR_TARGET}};${_file}.o)
+			endif()
+		endforeach()
+
+		#dprint("SRCS=${_SRCS}  objs_${_CNDR_TARGET}=${objs_${_CNDR_TARGET}}")
 		# putput from condor_compile
 		add_library( ${_CNDR_TARGET} STATIC EXCLUDE_FROM_ALL ${_SRCS})
 
-		command_target( cc_${_CNDR_TARGET} ${CONDOR_COMPILE} "-condor_lib;${STDU_LIB_LOC};-condor_ld_dir;${STDU_LIB_LOC};${_COMPILER};-o;${_CNDR_TARGET}.cndr.exe;lib${_CNDR_TARGET}.a;${_LINK_FLAGS}" "${_CNDR_TARGET}.cndr.exe" )
-		add_dependencies( cc_${_CNDR_TARGET} ${_CNDR_TARGET})
+		command_target( arx_${_CNDR_TARGET} ar "-x;lib${_CNDR_TARGET}.a" "${objs_${_CNDR_TARGET}}")
+		add_dependencies( arx_${_CNDR_TARGET} ${_CNDR_TARGET})
+
+		command_target( cc_${_CNDR_TARGET} ${CONDOR_COMPILE} "-condor_lib;${STDU_LIB_LOC};-condor_ld_dir;${STDU_LIB_LOC};${_COMPILER};-o;${_CNDR_TARGET}.cndr.exe;${objs_${_CNDR_TARGET}};${_LINK_FLAGS}" "${_CNDR_TARGET}.cndr.exe" )
+		add_dependencies( cc_${_CNDR_TARGET} arx_${_CNDR_TARGET})
 
 		command_target( ca_${_CNDR_TARGET} ${CONDOR_ARCH_LINK} "${_CNDR_TARGET}.cndr.exe" "${_CNDR_TARGET}.cndr.exe.Linux.${SYS_ARCH}")
 		add_dependencies( ca_${_CNDR_TARGET} cc_${_CNDR_TARGET})
 
-		#append_var(CONDOR_TESTS ca_${_CNDR_TARGET})
-		set_target_properties( cc_${_CNDR_TARGET} ca_${_CNDR_TARGET} PROPERTIES EXCLUDE_FROM_ALL TRUE)
+		append_var(CONDOR_TESTS ca_${_CNDR_TARGET})
+		set_target_properties( cc_${_CNDR_TARGET} ca_${_CNDR_TARGET} arx_${_CNDR_TARGET} PROPERTIES EXCLUDE_FROM_ALL TRUE)
 
 	endif()
 
