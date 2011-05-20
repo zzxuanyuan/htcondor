64a9e086aea90ae4a2767845668a5ccbf5d9aebf
Nathan W. Panike <nwp@cs.wisc.edu>
2011-05-20 11:35:44 -0500

    Use SSL version of base64 if we have it
    ===GT=== #2177
    	Otherwise, we use our home-grown one

--- src/condor_utils/condor_base64.cpp
+++ src/condor_utils/condor_base64.cpp
@@ -22,6 +22,66 @@
 #include "condor_config.h"
 #include "condor_base64.h"
 
+// For base64 encoding
+#if HAVE_EXT_OPENSSL
+# include <openssl/sha.h>
+# include <openssl/hmac.h>
+# include <openssl/evp.h>
+# include <openssl/bio.h>
+# include <openssl/buffer.h>
+
+// Caller needs to free the returned pointer
+char* condor_base64_encode(const unsigned char *input, int length)
+{
+	BIO *bmem, *b64;
+	BUF_MEM *bptr;
+
+	b64 = BIO_new(BIO_f_base64());
+	bmem = BIO_new(BIO_s_mem());
+	b64 = BIO_push(b64, bmem);
+	BIO_write(b64, input, length);
+	(void)BIO_flush(b64);
+	BIO_get_mem_ptr(b64, &bptr);
+
+	char *buff = (char *)malloc(bptr->length);
+	ASSERT(buff);
+	memcpy(buff, bptr->data, bptr->length-1);
+	buff[bptr->length-1] = 0;
+	BIO_free_all(b64);
+
+	return buff;
+}
+
+// Caller needs to free *output if non-NULL
+void condor_base64_decode(const char *input,unsigned char **output, int *output_length)
+{
+	BIO *b64, *bmem;
+
+	ASSERT( input );
+	ASSERT( output );
+	ASSERT( output_length );
+
+	int input_length = strlen(input);
+
+		// assuming output length is <= input_length
+	*output = (unsigned char *)malloc(input_length + 1);
+	ASSERT( *output );
+	memset(*output, 0, input_length);
+
+	b64 = BIO_new(BIO_f_base64());
+	bmem = BIO_new_mem_buf((void *)input, input_length);
+	bmem = BIO_push(b64, bmem);
+
+	*output_length = BIO_read(bmem, *output, input_length);
+	if( *output_length < 0 ) {
+		free( *output );
+		*output = NULL;
+	}
+
+	BIO_free_all(bmem);
+}
+
+#else // if !HAVE_EXT_OPENSSL
 
 namespace {
 static const char* b64_table = "ABCDEFGHIJKLMNOP"
@@ -137,3 +197,4 @@ void condor_base64_decode(const char *input,unsigned char **output, int *output_
 	} else 
 		*output_length = tolen;	
 }
+#endif // HAVE_EXT_OPENSSL
