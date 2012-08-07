4bb9de8ebf10bf489dcc3438fc12082bdd82b439
Guilherme Baptista <santos-g@cs.wisc.edu>
2012-08-07 18:48:07 -0500

    (#1156) Experimenting with Test framework
    Tests supposedly run on NMI after I added their entries.
    None of them passed, though. Yet, locally, they succeed.
    Comparing my tests with previous ones I notice there are some steps
    that I didn't add on.
    
    Now a submit file that points to test_name.cmd will run. I'm not sure if
    this will be enough to make it work.

---
+++ src/condor_tests/cmd_tools_cluster_multi-constraint.cmd
@@ -0,0 +1,10 @@
+universe   = vanilla
+executable = x_sleep.pl
+log = cmd_tools_cluster_multi-constraint.log
+output = cmd_tools_cluster_multi-constraint.out
+error = cmd_tools_cluster_multi-constraint.err
+Notification = NEVER
+hold = true
+arguments  = 0
+queue
+
