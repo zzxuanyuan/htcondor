0832c7d6526d15a791f66f8fb38c8a6f396113bf
Timothy St. Clair <tstclair@redhat.com>
2012-10-05 15:01:14 -0500

    Ticket #1282 It all builds with new json & c++ generator, now onto windows.

--- src/condor_utils/generate_params.cpp
+++ src/condor_utils/generate_params.cpp
@@ -1,17 +1,49 @@
 #include "picojson.h"
 #include <fstream>
+#include <sstream>
 #include <stdio.h>
 
 using namespace std;
 
 static bool bDebug=false;
 
+typedef enum dataType
+{
+    PARAM_TYPE_STRING = 0,
+    PARAM_TYPE_INT = 1,
+    PARAM_TYPE_BOOL = 2,
+    PARAM_TYPE_DOUBLE = 3
+}dType;
+	
 ///< Each parameter is a name=value pair.
 typedef struct tparam
 {
 	string _name;
 	string _value;
+	dType _type;
+	string range_low;
+	string range_high;
+	
 }tParam;
+
+/// escape a string for output purposes.
+string escape_str (const string & szIn)
+{
+    string szRet;
+    
+    for ( unsigned int i=0; i<szIn.length(); i++ )
+    {
+	// you could add in the other qualifiers if needed.
+	if ( szIn[i] == '\\' || szIn[i] == '\"' )
+	{
+	    szRet += '\\';
+	}
+	
+	szRet += szIn[i];
+    }
+    
+    return szRet;
+}
 
 /// Gets a string out of a single value if it is a string. 
 bool get_string(picojson::value o, string & szVal)
@@ -58,7 +90,6 @@ bool get_string_list(picojson::value o, vector<string>& szList)
 	cout<<"ERROR IN FILE FORMAT - NOT AN ARRAY OF STRINGS"<<endl;
     }
 	
-    
     return bRet;
 }
 
@@ -88,6 +119,7 @@ bool get_default(picojson::value o, string & szVal)
 #define NAME 		"name"
 #define NSPRINTF 	"nsprintf"
 #define VALUE		"value"
+#define TYPE            "type"
 
 bool parse_record( picojson::object o, vector< tParam > & table )
 {
@@ -96,29 +128,59 @@ bool parse_record( picojson::object o, vector< tParam > & table )
     vector<string> nsprintf;
     string name;
     string value;
+    string type;
     
     tParam param;
     
     // loop through the elements in each object switch on keywords.
     for (picojson::object::const_iterator j = o.begin(); j != o.end(); ++j)
     {
+	string key = j->first;
+	
 	if (bDebug)
 	{
 	    // extra output to check ones sanity.
-	    cout << "(" << j->first <<")-> "<<j->second<<endl;
+	    cout << "(" << key <<")-> "<<j->second<<endl;
 	}
 		    
-	if ( 0 == strcmp (j->first.c_str(), NAME) )
+	// here is where we check the keys
+	if ( 0 == strcasecmp (key.c_str(), NAME) )
 	{
 	    bRet = get_string(j->second, name);
 	}
-	else if ( 0 == strcmp (j->first.c_str(), NSPRINTF) )
+	else if ( 0 == strcasecmp (key.c_str(), NSPRINTF) )
 	{
 	    bRet = get_string_list(j->second, nsprintf);
 	}
-	else if ( 0 == strcmp (j->first.c_str(), VALUE ) )
+	else if ( 0 == strcasecmp (key.c_str(), VALUE ) )
 	{
 	    bRet = get_default(j->second, value);
+	}
+	else if ( 0 == strcasecmp (key.c_str(), TYPE ) )
+	{
+	    
+	    if ( (bRet = get_string(j->second, type) ) )
+	    { 
+		
+		if (0 == strcasecmp (type.c_str(), "int") )
+		{
+		    param._type = PARAM_TYPE_INT;
+		    
+		}else if (0 == strcasecmp (type.c_str(), "bool"))
+		{
+		      param._type = PARAM_TYPE_BOOL;
+		      
+		}else if (0 == strcasecmp (type.c_str(), "double"))
+		{
+		     param._type = PARAM_TYPE_DOUBLE;
+		     
+		}else 
+		{
+		    param._type = PARAM_TYPE_STRING;
+		}
+		
+	    }
+	    
 	}
 	
 	if (!bRet)
@@ -155,9 +217,7 @@ typedef struct tdefaults
 	
 	// This performs the useful part of loading the object.
 	bool load (const picojson::array& a) 
-	{
-	    bool bRet = true;
-	    
+	{   
 	    _defaults.clear();
 	    
 	    // loop through each object in the array/table
@@ -167,6 +227,7 @@ typedef struct tdefaults
 		picojson::value v = *i;
 		picojson::object o = v.get<picojson::object>();
 		
+		// parse each entry or record.
 		if ( !parse_record(o,_defaults) )
 		{
 		    return false;
@@ -180,14 +241,95 @@ typedef struct tdefaults
 	// This spews the contents out for the defaults table.
 	bool spew (const string szFileName)
 	{
-	    bool bRet = true;
 	    
+	    FILE * fOut = fopen(szFileName.c_str(), "w+");
+	    
+	    if (!fOut) 
+		return false;
+	    
+	    // write out the heading goo...
+	    fprintf(fOut, "#include \"param_info.h\"\n");
+	    fprintf(fOut, "void param_info_init()\n{\n");
+	    fprintf(fOut, "\tstatic int done_once = 0;\n\n");
+	    fprintf(fOut, "\tif (done_once == 1) {\n\t\treturn;\n\t}\n");
+	    fprintf(fOut, "\t\n\tdone_once = 1;\n\n\tparam_info_hash_create(&param_info);\n");
+	    fprintf(fOut, "\tparam_info_storage_t tmp;\n\n");
+	    
+	    cout<<"******DEFAULT PARAMS******"<<endl;
 	    for (unsigned int i=0; i<_defaults.size(); i++)
 	    {
-		cout<<_defaults[i]._name<<"="<<_defaults[i]._value<<endl;
+		const char * name = _defaults[i]._name.c_str();
+		const char * value = _defaults[i]._value.c_str();
+		string escaped = escape_str(_defaults[i]._value);
+		unsigned int str_len = _defaults[i]._value.length();
+		bool valid = str_len?true:false;
+		istringstream buffer(_defaults[i]._value);
+		
+		fprintf(fOut, "\ttmp.type_string.hdr.name = \"%s\";\n", name );
+		fprintf(fOut, "\ttmp.type_string.hdr.str_val = \"%s\";\n", escaped.c_str());
+		
+		cout<<name<<"="<<value<<" [type=";
+		switch ( _defaults[i]._type )
+		{
+		    case PARAM_TYPE_INT:
+			cout<<"int]"<<endl;
+			fprintf(fOut, "\ttmp.type_string.hdr.type = PARAM_TYPE_INT;\n"); 
+			int numb;
+			buffer >> numb;
+			
+			if (valid && !buffer.fail())
+			{
+			    fprintf(fOut, "\ttmp.type_int.int_val = %s;\n",value);
+			}
+			else
+			{
+			    valid = false;
+			}
+			    
+			break;
+		    case PARAM_TYPE_BOOL:
+			cout<<"bool]"<<endl;
+			fprintf(fOut, "\ttmp.type_string.hdr.type = PARAM_TYPE_BOOL;\n");
+			
+			if (0 == strcasecmp(value, "true"))
+			    fprintf(fOut, "\ttmp.type_int.int_val = true;\n");
+			else
+			    fprintf(fOut, "\ttmp.type_int.int_val = false;\n");
+			
+			break;
+		    case PARAM_TYPE_DOUBLE:
+			cout<<"double]"<<endl;
+			fprintf(fOut, "\ttmp.type_string.hdr.type = PARAM_TYPE_DOUBLE;\n");
+			double d;
+			
+			buffer >> d;
+			if (valid && !buffer.fail())
+			{
+			    fprintf(fOut, "\ttmp.type_double.dbl_val = %s;\n", value);
+			}
+			else
+			{
+			    valid = false;
+			}
+			
+			break;
+		    default:
+			cout<<"string]"<<endl;
+			fprintf(fOut, "\ttmp.type_string.hdr.type = PARAM_TYPE_STRING;\n");
+			
+		}
+		
+		fprintf(fOut, "\ttmp.type_string.hdr.default_valid = %d;\n", valid?1:0 );
+		fprintf(fOut, "\ttmp.type_string.hdr.range_valid = 0;\n");
+		fprintf(fOut, "\tparam_info_hash_insert(param_info, &tmp);\n\n");
+		
 	    }
 	    
-	    return bRet;
+	    cout<<"******END DEFAULT PARAMS******"<<endl;
+	    fprintf(fOut, "\tparam_info_hash_optimize(param_info);\n}\n\n");
+	    fclose(fOut);
+	    
+	    return true;
 	}
 	
 }tDefaultTable;
@@ -231,7 +373,8 @@ void print_parse_value (picojson::value obj_v, string szPrePend)
     
     }
 }
-//////////////////////////////////////////////////////////////////////
+//
+////////////////////////////////////////////////////////////////////
 
 int main(int argc, char *argv[])
 {
@@ -240,7 +383,12 @@ int main(int argc, char *argv[])
     fstream fParams_in;
     
     // load the defaults table.
-    fParams_in.open ("params.json.in", fstream::in | fstream::app);  
+    if (argc<3 || 0 != strcmp (argv[1],"-i"))
+    {
+	cerr << "invalid input: -i <filename> -debug"<<endl;
+    }
+    
+    fParams_in.open (argv[2], fstream::in | fstream::app);  
     fParams_in >> v;
     
     // check to see if we load the file. 
@@ -251,9 +399,9 @@ int main(int argc, char *argv[])
     }
   
     // used to debug if it is necessary
-    if (argc==2 ) 
+    if (argc==4) 
     {
-	if (0 == strcmp (argv[1],"--debug"))
+	if (0 == strcmp (argv[3],"-debug"))
 	{    
 	    cout<<"------------------------ BEGIN DEBUG PARSE PHASE ------------------------"<<endl;
 	    // this will test print out the object format during a parse
@@ -273,12 +421,14 @@ int main(int argc, char *argv[])
     if ( !default_table.load(a)  )
     {
 	cerr << "INVALID FORMAT"<<endl;
+	return 1;
     }
 	
     cout << "---- WRITING CONSUMABLE GOO ----" << endl;
     if ( !default_table.spew("param_info_init.c")  )
     {
-	cerr << "OHH I SUCK!!"<<endl;
+	cerr << "FAILED TO WRITE"<<endl;
+	return 1;
     }
     
     return 0;
