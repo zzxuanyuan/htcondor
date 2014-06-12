d1cd46db8d6f0a9b9264965b2dd6d451dde2d110
Derek Weitzel <djw8605@gmail.com>
2014-06-12 11:31:15 -0500

    Adding better messages to the cache tester

--- src/condor_contrib/condor_cached/condor_cached_tester.cpp
+++ src/condor_contrib/condor_cached/condor_cached_tester.cpp
@@ -19,26 +19,39 @@ main(int argc, char * argv[])
 	std::string cacheName = "/tester";
 	time_t expiry = time(NULL) + 86400;
 	int rc = client.createCacheDir(cacheName, expiry, err);
-	fprintf(stderr, "Return code from createCacheDir: %d\nError contents: %s\n", rc, err.getFullText().c_str());
 	if (rc) {
-		return 1;
+		fprintf(stderr, "FAIL: Return code from createCacheDir: %d\nError contents: %s\n", rc, err.getFullText().c_str());
+	} else {
+		fprintf(stdout, "SUCCESS: Initial creation of cache successful\n");
 	}
 
 	std::list<std::string> files;
 	files.push_front("/etc/hosts");
 	rc = client.uploadFiles(cacheName, files, err);
-	fprintf(stderr, "Return code from uploadFiles: %d\nError contents: %s\n", rc, err.getFullText().c_str());
 	if (rc) {
+		fprintf(stderr, "FAIL: Return code from uploadFiles: %d\nError contents: %s\n", rc, err.getFullText().c_str());
 		return 1;
+	} else {
+		fprintf(stdout, "SUCCESS: Initial upload of file successful\n");
+	}
+
+	// Attempt to upload the files again, should be an error:
+	rc = client.uploadFiles(cacheName, files, err);
+	if (rc) {
+		fprintf(stderr, "SUCCESS: Return code from uploadFiles: %d\nError contents: %s\n", rc, err.getFullText().c_str());
+	} else {
+		fprintf(stdout, "FAIL: Second upload of file successful, it shouldn't be...\n");
 	}
 
 
 	char destination[PATH_MAX];
 	getcwd(destination, PATH_MAX);
 	rc = client.downloadFiles(cacheName, destination, err);
-	fprintf(stderr, "Return code from downloadFiles: %d\nError contents: %s\n", rc, err.getFullText().c_str());
 	if (rc) {
+		fprintf(stderr, "FAIL: Return code from downloadFiles: %d\nError contents: %s\n", rc, err.getFullText().c_str());
 		return 1;
+	} else {
+		fprintf(stdout, "SUCCESS: Download of files successful\n");
 	}
 
 	return 0;
