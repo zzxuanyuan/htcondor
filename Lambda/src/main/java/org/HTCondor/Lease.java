3917989fe6f0d1c8d7979ae8b4d909a308073024
Todd L Miller <tlmiller@cs.wisc.edu>
2015-09-08 14:42:49 -0500

    (#5324)  A Lambda function called to destroy an AutoScaling Group when its lease expires.

--- Lambda/src/main/java/org/HTCondor/Lease.java
+++ Lambda/src/main/java/org/HTCondor/Lease.java
@@ -19,13 +19,28 @@ import com.amazonaws.services.autoscaling.model.*;
 // import com.amazonaws.services.cloudformation.*;
 
 public class Lease implements RequestHandler< SNSEvent, String > {
+	// FIXME: Handle (or handle in caller) ScalingActivityInProgressException
+	// and ResourceContentionException (via [Lambda-level] retries?).
+	// FIXME: Catch no-such-group exception and succeed.  (No point in
+	// retrying to delete a group that's already been deleted.)
 	private void deleteAutoScalingGroup( String asgName ) {
 		AmazonAutoScalingClient assc = new AmazonAutoScalingClient();
+
+		// We have to do this before actually deleting the ASG; otherwise,
+		// its instances could continue to run.
 		UpdateAutoScalingGroupRequest uasgr = new UpdateAutoScalingGroupRequest();
+		uasgr.setAutoScalingGroupName( asgName );
 		uasgr.setMinSize( 0 );
 		uasgr.setDesiredCapacity( 0 );
 		uasgr.setMaxSize( 0 );
 		assc.updateAutoScalingGroup( uasgr );
+
+		// Don't force the delete -- if the previous step failed to terminate
+		// all instances, we want the ASG to continue to exist so that the
+		// instances aren't lost.
+		DeleteAutoScalingGroupRequest dasgr = new DeleteAutoScalingGroupRequest();
+		dasgr.setAutoScalingGroupName( asgName );
+		assc.deleteAutoScalingGroup( dasgr );
 	}
 
 	private void handleRecord( SNSRecord record, Context context ) {
@@ -53,36 +68,59 @@ public class Lease implements RequestHandler< SNSEvent, String > {
 		// By convention, this will be embedded into the namespace of the
 		// metric.
 		//
+		JSONObject blob;
 		try {
-			JSONObject blob = new JSONObject( message );
-			JSONObject trigger = new JSONObject( blob.getString( "Trigger" ) );
-			String triggerNamespace = trigger.getString( "Namespace" );
-			logger.log( "Will parse trigger's namespace (" + triggerNamespace + ") to find target.\n" );
-
-			String[] nsEntries = triggerNamespace.split( "/" );
-			if( nsEntries.length != 4 ) {
-				logger.log( "Did not find the required number of pieces in the namespace.\n" );
-				return;
-			}
-			if(! nsEntries[0].equals( "HTCondor" )) {
-				logger.log( "Not an HTCondor metric (" + nsEntries[0] + "), ignoring.\n" );
-				return;
-			}
-			if(! nsEntries[1].equals( "Leases" )) {
-				logger.log( "Not a lease (" + nsEntries[1] + "), ignoring.\n" );
-				return;
-			}
-			// FIXME: uppercase 'S'.
-			if( nsEntries[2].equals( "AutoscalingGroup" ) ) {
-				logger.log( "Will delete ASG '" + nsEntries[3] + "'...\n" );
-				deleteAutoScalingGroup( nsEntries[2] );
-			} else {
-				logger.log( "Object of unknown type (" + nsEntries[2] + ") leased, ignoring.\n" );
-				return;
-			}
-
+			blob = new JSONObject( message );
 		} catch( JSONException je ) {
-			logger.log( "Failed to deblob (" + je.toString() + ").\n" );
+			logger.log( "Failed to deblob message (" + je.toString() + ").\n" );
+			return;
+		}
+
+		String triggerString;
+		try {
+			triggerString = blob.getString( "Trigger" );
+		} catch( JSONException je ) {
+			logger.log( "Message did not contain trigger.\n" );
+			return;
+		}
+
+		JSONObject trigger;
+		try {
+			trigger = new JSONObject( triggerString );
+		} catch( JSONException je ) {
+			logger.log( "Failed to deblob trigger (" + triggerString + ").\n" );
+			return;
+		}
+
+		String triggerNamespace;
+		try {
+			triggerNamespace = trigger.getString( "Namespace" );
+		} catch( JSONException je ) {
+			logger.log( "Trigger did not contain namespace.\n" );
+			return;
+		}
+		logger.log( "Will parse trigger's namespace (" + triggerNamespace + ") to find target.\n" );
+
+		String[] nsEntries = triggerNamespace.split( "/" );
+		if( nsEntries.length != 4 ) {
+			logger.log( "Did not find the required number of pieces in the namespace.\n" );
+			return;
+		}
+		if(! nsEntries[0].equals( "HTCondor" )) {
+			logger.log( "Not an HTCondor metric (" + nsEntries[0] + "), ignoring.\n" );
+			return;
+		}
+		if(! nsEntries[1].equals( "Leases" )) {
+			logger.log( "Not a lease (" + nsEntries[1] + "), ignoring.\n" );
+			return;
+		}
+
+		if( nsEntries[2].equals( "AutoScalingGroup" ) ) {
+			logger.log( "Will delete ASG '" + nsEntries[3] + "'...\n" );
+			deleteAutoScalingGroup( nsEntries[3] );
+		} else {
+			logger.log( "Object of unknown type (" + nsEntries[2] + ") leased, ignoring.\n" );
+			return;
 		}
 	}
 
