5bb6edbd7682ed26fb21ddc565521600bb61d79f
Todd L Miller <tlmiller@cs.wisc.edu>
2015-11-10 14:23:13 -0600

    (#5361)  Delete the pool password file only when deleting the stack.

--- src/condor_annex/Lambda/cleanup.js
+++ src/condor_annex/Lambda/cleanup.js
@@ -1,8 +1,9 @@
 var response = require( 'response' );
 exports.handler = function( event, context ) {
 
-	// console.log( "Received request:\n", JSON.stringify( event ) );
+	console.log( "Received request:\n", JSON.stringify( event ) );
 	if( event.RequestType != 'Delete' ) {
+		console.log( "Ignoring non-delete request." );
 		response.send( event, context, response.SUCCESS );
 		return;
 	}
@@ -17,10 +18,16 @@ exports.handler = function( event, context ) {
 	}
 
 	var s3PoolPassword = event.ResourceProperties.s3PoolPassword;
-	// ...
+	if(! s3PoolPassword) {
+		var responseData = { Error : 's3PoolPassword not specified' };
+		console.log( responseData.Error );
+		response.send( event, context, response.FAILED, responseData );
+		return;
+	}
 
 	var AWS = require( 'aws-sdk' );
 	var sns = new AWS.SNS();
+	var s3 = new AWS.S3();
 
 	function SendFailedResponse( error, message ) {
 		console.log( error, error.stack );
@@ -37,13 +44,13 @@ exports.handler = function( event, context ) {
 
 		var subs = data.Subscriptions;
 		if( subs.length == 0 ) {
-			// console.log( "... got zero subscriptions, nothing to do." );
+			console.log( "... got zero subscriptions, nothing to do." );
 			response.send( event, context, response.SUCCESS );
 			return;
 		}
 
 		var processed = 0;
-		// console.log( "... got subscriptions, removing them..." );
+		console.log( "... got subscriptions, removing them..." );
 		for( var i = 0; i < subs.length; ++i ) {
 			sns.unsubscribe( { SubscriptionArn : subs[i].SubscriptionArn }, function( error, data ) {
 				if( error ) {
@@ -53,13 +60,26 @@ exports.handler = function( event, context ) {
 
 				++processed;
 				if( processed == subs.length ) {
-					// console.log( "... removed all subscriptions." );
+					console.log( "... removed all subscriptions." );
 					response.send( event, context, response.SUCCESS );
 				}
 			} );
 		}
 	};
 
-	// console.log( "Looking up subscriptions..." );
-	sns.listSubscriptionsByTopic( { TopicArn : topicARN }, lsbtFunction );
+
+	var names = s3PoolPassword.split( '/' );
+
+	console.log( "Deleting pool password file..." );
+	s3.deleteObject( { Bucket : names[0], Key : names[1] }, function( error, data ) {
+		if( error ) {
+			SendFailedResponse( error, 'delete() call failed' );
+			return;
+		} else {
+			console.log( "... pool password file deleted." );
+			console.log( "Looking up subscriptions..." );
+			sns.listSubscriptionsByTopic( { TopicArn : topicARN }, lsbtFunction );
+		}
+	} );
+
 }
