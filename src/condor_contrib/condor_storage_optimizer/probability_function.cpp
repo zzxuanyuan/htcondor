295d09927ba34f5d747b956319befb51b6c1fc58
Zhe Zhang <zzxuanyuan@gmail.com>
2018-11-12 04:41:23 +0000

    Temporarily change failure rate to 0.4

--- src/condor_contrib/condor_storage_optimizer/probability_function.cpp
+++ src/condor_contrib/condor_storage_optimizer/probability_function.cpp
@@ -13,6 +13,10 @@ ProbabilityFunction::ProbabilityFunction(DISTRIBUTION_TYPE type)
 double ProbabilityFunction::getProbability(double time_to_fail)
 {
 	// We will replace this with a function which calculate pdf
-	double failure_rate = (double)rand() / (double)RAND_MAX;
+//	double failure_rate = (double)rand() / (double)RAND_MAX;
+	// TODO: temporarily set failure_rate to 0.4 and make sure there will be always three redundanduncy
+	// selected given max_failure_rate = 0.1. This is for testing purpose and should be switched back to
+	// random failure rate or other distribution
+	double failure_rate = 0.4;
 	return failure_rate;
 }
