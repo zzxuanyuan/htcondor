ac52f0646da31bac38edeff4a276508b1dc13535
Zhe Zhang <zzxuanyuan@gmail.com>
2019-11-17 03:29:12 +0000

    Correct the number of parameters in getMostReliableCacheD

--- src/python-bindings/cached.cpp
+++ src/python-bindings/cached.cpp
@@ -427,7 +427,7 @@ struct Cached {
 
 BOOST_PYTHON_MEMBER_FUNCTION_OVERLOADS(listCacheDirs_overloads, listCacheDirs, 0, 2);
 BOOST_PYTHON_MEMBER_FUNCTION_OVERLOADS(listCacheDs_overloads, listCacheDs, 0, 1);
-BOOST_PYTHON_MEMBER_FUNCTION_OVERLOADS(getMostReliableCacheD_overloads, getMostReliableCacheD, 0, 2);
+BOOST_PYTHON_MEMBER_FUNCTION_OVERLOADS(getMostReliableCacheD_overloads, getMostReliableCacheD, 0, 3);
 
 void export_cached()
 {
@@ -485,6 +485,7 @@ void export_cached()
         .def("getMostReliableCacheD", &Cached::getMostReliableCacheD, getMostReliableCacheD_overloads("Get the most reliable Cache Classad\n"
             ":param timeToFailureMinutes: required time to failure minutes for the cache\n"
             ":param cacheSize: cache size\n"
+            ":param locationBlockout\n"
             ":return: A classad describing the returned CacheD\n"))
         .def("encodeDir", &Cached::encodeDir, "Encoding a directory\n"
             ":param cacheServer: Server\n"
